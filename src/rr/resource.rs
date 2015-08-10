e617843e4f3dd191dc38de6ec1fd8457e98899cc
Benjamin Fry <benjaminfry@me.com>
2015-08-10 14:49:52 -0700

    Compiling!

--- src/rr/resource.rs
+++ src/rr/resource.rs
@@ -1,10 +1,11 @@
 use std::collections::{VecDeque,HashMap};
 use std::iter::Take;
 use std::slice::Iter;
+use std::error::Error;
+use std::string::FromUtf8Error;
 
 use super::record_type::RecordType;
 use super::dns_class::DNSClass;
-
 
 // labels          63 octets or less
 // names           255 octets or less
@@ -31,13 +32,12 @@ impl Record {
   /// parses the chain of labels
   ///  this has a max of 255 octets, with each label being less than 63.
   ///  all names will be stored lowercase internally.
-  fn parse_labels(slice: &[u8]) -> VecDeque<String> {
+  fn parse_labels(slice: &[u8]) -> Result<VecDeque<String>, FromUtf8Error> {
     let mut state: LabelParseState = LabelParseState::LabelLengthOrPointer;
-    let mut labels: VecDeque<String>;
+    let mut labels: VecDeque<String> = VecDeque::with_capacity(3); // most labels will be around three, e.g. www.example.com
 //    let mut label_map: HashMap<u8, usize>;  // temporary map for pointers, offset in slice, start location in labels
 
-    // get the iterator for the slice, we'll use this for getting all the data
-    let iter: Iter<_> = slice.iter();
+    let mut cur_slice = slice;
 
     // assume all chars are utf-8. We're doing byte-by-byte operations, no endianess issues...
     // reserved: (1000 0000 aka 0800) && (0100 0000 aka 0400)
@@ -47,27 +47,30 @@ impl Record {
     loop {
       state = match state {
         LabelParseState::LabelLengthOrPointer => {
-          let byte: u8 = *iter.take(1).next().unwrap(); // could default to zero, but it's an error if this doesn't exist, perhaps try! instead
+          //let byte: u8 = *iter.take(1).next().unwrap(); // could default to zero, but it's an error if this doesn't exist, perhaps try! instead
+          let (first, cur_slice) = cur_slice.split_at(1);
 
-          //
-          match byte {
-            0 => LabelParseState::Root,
-            byte if byte & 0xC0 == 0xC0 => LabelParseState::Pointer(byte & 0x3F),
-            byte if byte <= 0x3F        => LabelParseState::Label(byte),
+          match first.first() {
+            Some(&0) | None => LabelParseState::Root,
+            Some(&byte) if byte & 0xC0 == 0xC0 => LabelParseState::Pointer(byte & 0x3F),
+            Some(&byte) if byte <= 0x3F        => LabelParseState::Label(byte),
             _ => unimplemented!(),
           }
           },
         LabelParseState::Label(count) => {
-          let label_iter: Take<Iter<u8>> = iter.take(count as usize);
-          let arr: Vec<u8> = label_iter.collect();
+          //let label_iter: Take<Iter<u8>> = iter.take(count as usize);
+          //let arr: Vec<&u8> = label_iter.collect();
+          //let arr2: Vec<u8> = arr.to_vec();
+          let (label_slice, cur_slice) = cur_slice.split_at(count as usize);
+
 
           // using lossy, this is safe, but can end up with junk in the name...
           // TODO other option
-          let label = try!(String::from_utf8(arr));
+          let label = try!(String::from_utf8(label_slice.into()));
           labels.push_back(label);
 
           // reset to collect more data
-          LabelParseState::LabelLengthOrPointer()
+          LabelParseState::LabelLengthOrPointer
           },
         LabelParseState::Pointer(offset) => {
           // lookup in the hashmap the label to use
@@ -80,7 +83,7 @@ impl Record {
       }
     }
 
-    return labels;
+    return Ok(labels);
   }
 }
 
@@ -90,14 +93,4 @@ enum LabelParseState {
   Label(u8),   // storing length of the label, must be < 63
   Pointer(u8), // location of pointer in slice,
   Root,        // root is the end of the labels list, aka null
-}
-
-impl LabelParseState {
-  fn take_count(&self) -> u8 {
-    match *self {
-      LabelParseState::LabelLengthOrPointer => 1,
-      LabelParseState::Label => *self, // unwrap the enum, this is the length of the label
-      LabelParseState::Pointer => *self,
-    }
-  }
 }
