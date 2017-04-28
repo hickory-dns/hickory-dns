4a22016f3d6867d017d5f07c98843a3b70e4f0a0
Jan Niehusmann <jan@gondor.com>
2017-04-29 00:50:41 +0200

    Avoid code duplication in Message::sign()
    Use new function `RData::SIG::set_sig(self, signature: Vec<u8>) -> Self {...}`,
    instead.

--- client/src/op/message.rs
+++ client/src/op/message.rs
@@ -655,25 +655,7 @@ impl Message {
                                 signer.signer_name().clone(),
                                 Vec::new());
         let signature: Vec<u8> = try!(signer.sign_message(self, &pre_sig0));
-        sig0.set_rdata(
-            RData::SIG(SIG::new(
-                // type covered in SIG(0) is 0 which is what makes this SIG0 vs a standard SIG
-                RecordType::NULL,
-                signer.algorithm(),
-                num_labels,
-                // see above, original_ttl is meaningless, The TTL fields SHOULD be zero
-                0,
-                // recommended time is +5 minutes from now, to prevent timing attacks, 2 is probably good
-                expiration_time,
-                // current time, this should be UTC
-                // unsigned numbers of seconds since the start of 1 January 1970, GMT
-                inception_time,
-                key_tag,
-                // can probably get rid of this clone if the owndership is correct
-                signer.signer_name().clone(),
-                signature,
-            )
-        ));
+        sig0.set_rdata(RData::SIG(pre_sig0.set_sig(signature)));
 
         debug!("sig0: {:?}", sig0);
 
