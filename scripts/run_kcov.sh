1bc104a293b436f189645d7d74ac8548403d47e1
Benjamin Fry <benjaminfry@me.com>
2017-05-02 05:32:38 -0700

    add path to upload option

--- scripts/run_kcov.sh
+++ scripts/run_kcov.sh
@@ -54,8 +54,8 @@ echo "----> ran $test_count test(s)"
 echo "----> merging and uploading to coveralls.io"
 
 if [[ "$test_count" -eq 1 ]] ; then 
-  echo kcov --coveralls-id=${TRAVIS_JOB_ID}
-  kcov --coveralls-id=${TRAVIS_JOB_ID}
+  echo kcov --coveralls-id=${TRAVIS_JOB_ID} target/kcov-*
+  kcov --coveralls-id=${TRAVIS_JOB_ID} target/kcov-*
 elif [[ "$test_count" -gt 1 ]] ; then
   echo kcov --coveralls-id=${TRAVIS_JOB_ID} --merge target/kcov-*
   kcov --coveralls-id=${TRAVIS_JOB_ID} --merge target/kcov-*
