cec1373dba5ccc73402e23f40e7db12c31f2e7ee
Benjamin Fry <benjaminfry@me.com>
2017-07-03 23:19:28 -0700

    ignore bad example doc

--- resolver/src/lib.rs
+++ resolver/src/lib.rs
@@ -94,7 +94,7 @@
 //!
 //! Generaally after a lookup in an asynchornous context, there would probably be a connection made to a server, for example:
 //!
-//! ```rust,no_compile
+//! ```c
 //! let result = io_loop.run(lookup_future.and_then(|ips| {
 //!                              let ip = ips.next().unwrap();
 //!                              TcpStream::connect()
