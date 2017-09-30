fd98c080d648826c53e459fb5c9f880d303df04b
Benjamin Fry <benjaminfry@me.com>
2017-09-30 10:16:37 -0700

    sha512 test

--- server/tests/named_test_configs/dnssec/gen-keys.sh
+++ server/tests/named_test_configs/dnssec/gen-keys.sh
@@ -6,10 +6,8 @@ OPENSSL=/usr/local/opt/openssl/bin/openssl
 KT=kt
 
 RSA_2048=rsa_2048.pem
-RSA_2048_PUB=rsa_2048_pub.pem
 
 [ -f ${RSA_2048:?} ] || ${OPENSSL:?} genrsa -des3 -out ${RSA_2048:?} 2048
-#[ -f ${RSA_2048_PUB:?} ] || ${OPENSSL:?} rsa -in ${RSA_2048:?} -outform PEM -pubout -out ${RSA_2048_PUB:?}
 
 ED25519=ed25519.pk8
 
