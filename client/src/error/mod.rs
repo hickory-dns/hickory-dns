976a2746b33c9f6607e162b080d8586b4064fe54
Benjamin Fry <benjaminfry@me.com>
2017-10-17 07:51:51 -0700

    remove commented out errors

--- client/src/error/mod.rs
+++ client/src/error/mod.rs
@@ -18,31 +18,26 @@
 //! All defined errors for Trust-DNS
 
 mod dnssec_error;
-//mod encode_error;
 mod client_error;
 mod lexer_error;
 mod parse_error;
 
 pub use self::dnssec_error::Error as DnsSecError;
-//pub use self::encode_error::Error as EncodeError;
 pub use self::client_error::Error as ClientError;
 pub use self::lexer_error::Error as LexerError;
 pub use self::parse_error::Error as ParseError;
 
 pub use self::dnssec_error::ErrorKind as DnsSecErrorKind;
-//pub use self::encode_error::ErrorKind as EncodeErrorKind;
 pub use self::client_error::ErrorKind as ClientErrorKind;
 pub use self::lexer_error::ErrorKind as LexerErrorKind;
 pub use self::parse_error::ErrorKind as ParseErrorKind;
 
 pub use self::dnssec_error::ChainErr as DnsSecChainErr;
-//pub use self::encode_error::ChainErr as EncodeChainErr;
 pub use self::client_error::ChainErr as ClientChainErr;
 pub use self::lexer_error::ChainErr as LexerChainErr;
 pub use self::parse_error::ChainErr as ParseChainErr;
 
 pub use self::dnssec_error::Result as DnsSecResult;
-//pub type EncodeResult = self::encode_error::Result<()>;
 pub use self::client_error::Result as ClientResult;
 pub use self::lexer_error::Result as LexerResult;
 pub use self::parse_error::Result as ParseResult;
