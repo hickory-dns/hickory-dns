c567e0ef861857793388550a8bc612feb80ea6e0
Benjamin Fry <benjaminfry@me.com>
2017-10-26 22:20:23 -0700

    remove commented out coveralls

--- scripts/run_kcov.sh
+++ scripts/run_kcov.sh
@@ -55,15 +55,6 @@ done
 
 echo "----> ran $test_count test(s)"
 
-# echo "----> merging and uploading to coveralls.io"
-# if [[ "$test_count" -eq 1 ]] ; then
-#   echo kcov --coveralls-id=${TRAVIS_JOB_ID} --report-only target/kcov-* ${last_test}
-#   kcov --coveralls-id=${TRAVIS_JOB_ID} --report-only target/kcov-* ${last_test}
-# elif [[ "$test_count" -gt 1 ]] ; then
-#   echo kcov --coveralls-id=${TRAVIS_JOB_ID} --report-only --merge target/kcov-*
-#   kcov --coveralls-id=${TRAVIS_JOB_ID} --report-only --merge target/kcov-*
-# fi
-
 echo "----> uploading to codecov.io"
 bash <(curl -s https://codecov.io/bash)
 echo "----> coverage reports done"
