d05f1a8cc85d66c69034692cd8dd84e65c3217c4
Benjamin Fry <benjaminfry@me.com>
2017-11-09 23:48:04 -0800

    switch to RSA keys b/c of Apple for Native-TLS

--- server/tests/z_named_native_tls_tests.rs
+++ server/tests/z_named_native_tls_tests.rs
@@ -5,7 +5,6 @@
 // http://opensource.org/licenses/MIT>, at your option. This file may not be
 // copied, modified, or distributed except according to those terms.
 
-#![cfg(feature = "bug#255")]
 #![cfg(not(windows))]
 #![cfg(feature = "tls")]
 
