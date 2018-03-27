867d4b25582639366c371f725f3163136282afc7
Benjamin Fry <benjaminfry@me.com>
2018-03-27 15:16:00 -0700

    refactor DnsFuture for multiple responses

---
+++ proto/src/xfer/dns_request.rs
@@ -0,0 +1,41 @@
+// Copyright 2015-2018 Benjamin Fry <benjaminfry@me.com>
+//
+// Licensed under the Apache License, Version 2.0, <LICENSE-APACHE or
+// http://apache.org/licenses/LICENSE-2.0> or the MIT license <LICENSE-MIT or
+// http://opensource.org/licenses/MIT>, at your option. This file may not be
+// copied, modified, or distributed except according to those terms.
+
+use smallvec::SmallVec;
+
+use op::Message;
+use rr::RecordType;
+
+/// A set of options for expressing options to how requests should be treated
+#[derive(Default)]
+pub struct DnsRequestOptions {
+    /// If true, then the request will block until all reqeusts have been received
+    pub expects_multiple_responses: bool,
+    /// If set, then the request will terminate early if all types have been received
+    pub expected_record_types: Option<SmallVec<[RecordType; 2]>>,
+}
+
+/// A DNS reqeust object
+pub struct DnsRequest {
+    message: Message,
+    options: DnsRequestOptions,
+}
+
+impl DnsRequest {
+    /// Returns a new DnsRequest object
+    pub fn new(message: Message, options: DnsRequestOptions) -> Self {
+        DnsRequest { message, options }
+    }
+
+    pub fn message(&self) -> &Message {
+        &self.message
+    }
+
+    pub fn options(&self) -> &DnsRequestOptions {
+        &self.options
+    }
+}
--- proto/src/xfer/mod.rs
+++ proto/src/xfer/mod.rs
@@ -2,11 +2,16 @@
 
 use std::fmt::Debug;
 
-pub mod dns_handle;
 pub mod dns_future;
+pub mod dns_handle;
+pub mod dns_request;
 pub mod retry_dns_handle;
 #[cfg(feature = "dnssec")]
 pub mod secure_dns_handle;
+
+pub use self::dns_future::DnsFuture;
+pub use self::dns_handle::{BasicDnsHandle, DnsHandle, StreamHandle};
+pub use self::dns_request::{DnsRequest, DnsRequestOptions};
 
 /// Ignores the result of a send operation and logs and ignores errors
 fn ignore_send<M, E: Debug>(result: Result<M, E>) {
