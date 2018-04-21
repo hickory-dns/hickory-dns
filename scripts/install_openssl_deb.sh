b6d2d896037a3b94ab690fba7f45436919a82a76
Benjamin Fry <benjaminfry@me.com>
2018-04-21 08:40:37 -0700

    update openssl installation

--- scripts/install_openssl_deb.sh
+++ scripts/install_openssl_deb.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-wget http://security.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.0.0_1.0.2g-1ubuntu4.10_amd64.deb
-wget http://security.ubuntu.com/ubuntu/pool/main/o/openssl/libssl-dev_1.0.2g-1ubuntu4.10_amd64.deb
+readonly UBUNTU_VERSION=1ubuntu4.12
 
-sudo dpkg -i libssl1.0.0_1.0.2g-1ubuntu4.10_amd64.deb
-sudo dpkg -i libssl-dev_1.0.2g-1ubuntu4.10_amd64.deb
+wget http://security.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.0.0_1.0.2g-${UBUNTU_VERSION:?}_amd64.deb
+wget http://security.ubuntu.com/ubuntu/pool/main/o/openssl/libssl-dev_1.0.2g-${UBUNTU_VERSION:?}_amd64.deb
+
+sudo dpkg -i libssl1.0.0_1.0.2g-${UBUNTU_VERSION:?}_amd64.deb
+sudo dpkg -i libssl-dev_1.0.2g-${UBUNTU_VERSION:?}_amd64.deb
