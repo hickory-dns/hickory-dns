5b80e9c3407a1eebbace859939a633bc6761003e
Benjamin Fry <benjaminfry@me.com>
2018-04-24 00:13:15 -0700

    reduce matrix size

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -1,18 +1,48 @@
-#!/bin/bash -e
+#!/bin/bash
 
-MODULES=${MODULES:-"client integration-tests native-tls openssl proto resolver rustls server"}
-CLIENT_OPTIONS=${CLIENT_OPTIONS} # add in all features
-OPTIONS=${OPTIONS}
+# Enumerates all tests and feature variations for each module
 
-trust_dns_dir=$(dirname $0)/..
-cd ${trust_dns_dir:?}
+# trust-dns-proto
+cargo test --manifest-path proto/Cargo.toml
+cargo test --manifest-path proto/Cargo.toml --all-features
+cargo test --manifest-path proto/Cargo.toml --no-default-features
+cargo test --manifest-path proto/Cargo.toml --no-default-features --features=dnssec-openssl
+cargo test --manifest-path proto/Cargo.toml --no-default-features --features=dnssec-ring
+  
+# trust-dns
+cargo test --manifest-path client/Cargo.toml
+cargo test --manifest-path client/Cargo.toml --all-features
+cargo test --manifest-path client/Cargo.toml --no-default-features
+cargo test --manifest-path client/Cargo.toml --no-default-features --features=dnssec-openssl
+cargo test --manifest-path client/Cargo.toml --no-default-features --features=dnssec-ring
 
-for i in ${MODULES:?}; do
-  pushd $i
-  opts=${OPTIONS}
-  if [ $i == "client" ] ; then opts="${OPTIONS} ${CLIENT_OPTIONS}" ; fi
-    
-  echo "executing $i: cargo test ${opts} $@"
-  cargo test ${opts} $@
-  popd
-done
+# trust-dns-*tls
+cargo test --manifest-path native-tls/Cargo.toml
+cargo test --manifest-path openssl/Cargo.toml
+cargo test --manifest-path rustls/Cargo.toml
+
+# trust-dns-resolver
+cargo test --manifest-path resolver/Cargo.toml
+cargo test --manifest-path resolver/Cargo.toml --all-features
+cargo test --manifest-path resolver/Cargo.toml --no-default-features
+cargo test --manifest-path resolver/Cargo.toml --no-default-features --features=dnssec-openssl
+cargo test --manifest-path resolver/Cargo.toml --no-default-features --features=dnssec-ring
+cargo test --manifest-path resolver/Cargo.toml --no-default-features --features=dns-over-native-tls
+cargo test --manifest-path resolver/Cargo.toml --no-default-features --features=dns-over-openssl
+cargo test --manifest-path resolver/Cargo.toml --no-default-features --features=dns-over-rustls
+
+# trust-dns-server
+cargo test --manifest-path server/Cargo.toml
+cargo test --manifest-path server/Cargo.toml --all-features
+cargo test --manifest-path server/Cargo.toml --no-default-features
+cargo test --manifest-path server/Cargo.toml --no-default-features --features=dnssec-openssl
+cargo test --manifest-path server/Cargo.toml --no-default-features --features=dnssec-ring
+cargo test --manifest-path server/Cargo.toml --no-default-features --features=dns-over-openssl
+
+# trust-dns-integration-tests
+cargo test --manifest-path integration-tests/Cargo.toml
+cargo test --manifest-path integration-tests/Cargo.toml --all-features
+cargo test --manifest-path integration-tests/Cargo.toml --no-default-features
+cargo test --manifest-path integration-tests/Cargo.toml --no-default-features --features=dnssec-openssl
+cargo test --manifest-path integration-tests/Cargo.toml --no-default-features --features=dnssec-ring
+cargo test --manifest-path integration-tests/Cargo.toml --no-default-features --features=dns-over-openssl
