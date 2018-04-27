3c8efa0eb0fec7577d3e969104497358f05c73f4
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2018-04-27 19:34:50 +0200

    hide tokio_reactor::Handle as much as possible
    Signed-off-by: Marc-Antoine Perennou <Marc-Antoine@Perennou.com>

--- client/src/multicast/mdns_client_connection.rs
+++ client/src/multicast/mdns_client_connection.rs
@@ -11,7 +11,6 @@ use std::io;
 use std::net::{Ipv4Addr, SocketAddr};
 
 use futures::Future;
-use tokio_reactor::Handle;
 use trust_dns_proto::DnsStreamHandle;
 
 use client::ClientConnection;
@@ -66,7 +65,6 @@ impl ClientConnection for MdnsClientConnection {
             self.packet_ttl,
             self.ipv4_if,
             self.ipv6_if,
-            &Handle::current(),
         );
 
         Ok((mdns_client_stream, handle))
