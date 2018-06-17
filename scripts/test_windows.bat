2ef9dd94c2b8670a9586e543e3766112c5302132
Benjamin Fry <benjaminfry@me.com>
2018-06-16 20:16:27 -0700

    enable automated tests for https

--- scripts/test_windows.bat
+++ scripts/test_windows.bat
@@ -52,6 +52,7 @@ if [%DNS_OVER_TLS_SUITE%] EQU [1] (
     cargo test --manifest-path native-tls\Cargo.toml
     cargo test --manifest-path openssl\Cargo.toml
     cargo test --manifest-path rustls\Cargo.toml
+    cargo test --manifest-path https\Cargo.toml
     cargo test --manifest-path resolver\Cargo.toml --features=dns-over-native-tls
     cargo test --manifest-path resolver\Cargo.toml --features=dns-over-openssl
     cargo test --manifest-path server\Cargo.toml --features=dns-over-openssl
