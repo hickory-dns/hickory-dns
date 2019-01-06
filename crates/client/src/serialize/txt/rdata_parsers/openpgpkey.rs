d8091c36e864d3a2f0b8a996ac41c8085a8f8db8
Alexander Esgen <alexander@esgen.org>
2019-01-06 09:07:32 +0100

    fix representation format & add tests

--- crates/client/src/serialize/txt/rdata_parsers/openpgpkey.rs
+++ crates/client/src/serialize/txt/rdata_parsers/openpgpkey.rs
@@ -1,4 +1,9 @@
-// TODO license
+// Copyright 2019 Benjamin Fry <benjaminfry@me.com>
+//
+// Licensed under the Apache License, Version 2.0, <LICENSE-APACHE or
+// http://apache.org/licenses/LICENSE-2.0> or the MIT license <LICENSE-MIT or
+// http://opensource.org/licenses/MIT>, at your option. This file may not be
+// copied, modified, or distributed except according to those terms.
 
 //! OPENPGPKEY records for OpenPGP public keys
 
@@ -17,16 +22,26 @@ use rr::rdata::OPENPGPKEY;
 ///    Section 11.1 of [RFC4880] encoded in base64 as defined in Section 4
 ///    of [RFC4648].
 /// ```
-pub fn parse<'i, I: Iterator<Item = &'i str>>(tokens: I) -> ParseResult<OPENPGPKEY> {
-    data_encoding::BASE64
-        .decode(tokens.collect::<String>().as_bytes())
-        .map_err(Into::into)
-        .and_then(|public_key| {
-            Some(public_key).filter(|s| !s.is_empty()).ok_or_else(|| {
-                ParseErrorKind::Message("OPENPGPKEY public key field is missing").into()
-            })
-        })
-        .map(OPENPGPKEY::new)
+pub fn parse<'i, I: Iterator<Item = &'i str>>(mut tokens: I) -> ParseResult<OPENPGPKEY> {
+    let encoded_public_key = tokens
+        .next()
+        .ok_or_else(|| ParseErrorKind::Message("OPENPGPKEY public key field is missing"))?;
+    let public_key = ::data_encoding::BASE64.decode(encoded_public_key.as_bytes())?;
+    Some(OPENPGPKEY::new(public_key))
+        .filter(|_| tokens.next().is_none())
+        .ok_or_else(|| ParseErrorKind::Message("too many fields for OPENPGPKEY").into())
 }
 
-// TODO test
+#[test]
+fn test_parsing() {
+    assert!(parse(::std::iter::empty()).is_err());
+    assert!(parse(vec!["äöüäööüä"].into_iter()).is_err());
+    assert!(parse(vec!["ZmFpbGVk", "äöüäöüö"].into_iter()).is_err());
+
+    assert!(parse(vec!["dHJ1c3RfZG5zIGlzIGF3ZXNvbWU="].into_iter())
+        .map(|rd| rd == OPENPGPKEY::new("trust_dns is awesome".as_bytes().to_vec()))
+        .unwrap_or(false));
+    assert!(parse(vec!["c2VsZi1wcmFpc2Ugc3Rpbmtz"].into_iter())
+        .map(|rd| rd == OPENPGPKEY::new("self-praise stinks".as_bytes().to_vec()))
+        .unwrap_or(false));
+}
