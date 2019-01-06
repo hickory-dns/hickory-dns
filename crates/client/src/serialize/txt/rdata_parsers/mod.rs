378831049be6182749b4ac2d4e4922cb9137232f
Alexander Esgen <alexander@esgen.org>
2019-01-06 09:19:53 +0100

    update copyright + minor things
     - removed println from TLSA presentation parsing
     - added doc for the Canonical DNS Name Order

--- crates/client/src/serialize/txt/rdata_parsers/mod.rs
+++ crates/client/src/serialize/txt/rdata_parsers/mod.rs
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2015 Benjamin Fry <benjaminfry@me.com>
+ * Copyright (C) 2015-2019 Benjamin Fry <benjaminfry@me.com>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
