72d7c19e852499e8616fe553ea945eb58c9cec52
Benjamin Fry <benjaminfry@me.com>
2019-01-12 11:33:37 -0800

    install openssl from chocolatey

--- scripts/install_openssl_win.sh
+++ scripts/install_openssl_win.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-BITS=64
-TARGET=x86_64-pc-windows-msvc
-OPENSSL_VERSION=1_1_0j
+# BITS=64
+# TARGET=x86_64-pc-windows-msvc
+# OPENSSL_VERSION=1_1_0j
 
-wget -nv "http://slproweb.com/download/Win${BITS}OpenSSL-${OPENSSL_VERSION}.exe"
-./Win${BITS}OpenSSL-${OPENSSL_VERSION}.exe /SILENT /VERYSILENT /SP- /DIR='C:\\OpenSSL'
-wget -nv "https://curl.haxx.se/ca/cacert.pem" -O 'C:\\OpenSSL\\cacert.pem'
+# wget -nv "http://slproweb.com/download/Win${BITS}OpenSSL-${OPENSSL_VERSION}.exe"
+# ./Win${BITS}OpenSSL-${OPENSSL_VERSION}.exe /SILENT /VERYSILENT /SP- /DIR='C:\\OpenSSL'
+# wget -nv "https://curl.haxx.se/ca/cacert.pem" -O 'C:\\OpenSSL\\cacert.pem'
+
+choco install openssl.light
