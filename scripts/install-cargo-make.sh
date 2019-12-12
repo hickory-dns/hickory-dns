33d05b0e93fc7ce02028ff7b8eb7de22d23c6078
Benjamin Fry <benjaminfry@me.com>
2019-12-12 00:24:42 -0800

    Script for installing cargo make

--- scripts/install-cargo-make.sh
+++ scripts/install-cargo-make.sh
@@ -5,12 +5,14 @@ set -e
 ## This must run after OpenSSL installation
 CARGO_MAKE_VER=0.24.1
 
+cargo --version
+
 # if it's already installed, skip
-#if cargo make --version ; then exit 0 ; fi
+if cargo make --version ; then exit 0 ; fi
 
 if ver ; then OS="windows" ;
-elif [[ "$(uname)" == "Darwin" ]] ; then OS="darwin" ;
-elif [[ "$(uname)" == "Linux" ]] ; then OS="linux" ;
+elif [ "$(uname)" == "Darwin" ] ; then OS="darwin" ;
+elif [ "$(uname)" == "Linux" ] ; then OS="linux" ;
 else OS=unknown ;
 fi
 
@@ -22,6 +24,7 @@ case $OS in
         wget -O cargo-make.zip ${CARGO_MAKE_URL:?}
         unzip cargo-make.zip
         
+        mkdir -p ${HOME}/.cargo/bin
         cp -n cargo-make-v${CARGO_MAKE_VER:?}-pc-windows-msvc/cargo-make ${HOME}/.cargo/bin/cargo-make
     ;;
     "darwin")
@@ -31,6 +34,7 @@ case $OS in
         wget -O cargo-make.zip ${CARGO_MAKE_URL:?}
         unzip cargo-make.zip
 
+        mkdir -p ${HOME}/.cargo/bin/
         cp -n cargo-make-v${CARGO_MAKE_VER:?}-x86_64-apple-darwin/cargo-make ${HOME}/.cargo/bin/cargo-make
     ;;
     "linux")
@@ -40,6 +44,7 @@ case $OS in
         wget -O cargo-make.zip ${CARGO_MAKE_URL:?}
         unzip cargo-make.zip
 
+        mkdir -p ${HOME}/.cargo/bin/
         cp -n cargo-make-v${CARGO_MAKE_VER:?}-x86_64-unknown-linux-musl ${HOME}/.cargo/bin/cargo-make
     ;;
     *)
