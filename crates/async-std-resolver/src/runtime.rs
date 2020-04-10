405fbfb99a7866d9a1f47d8ca519838d191d46f6
Benjamin Fry <benjaminfry@me.com>
2020-04-10 14:57:29 -0700

    port over all tests

--- crates/async-std-resolver/src/runtime.rs
+++ crates/async-std-resolver/src/runtime.rs
@@ -1,3 +1,10 @@
+// Copyright 2015-2020 Benjamin Fry <benjaminfry@me.com>
+//
+// Licensed under the Apache License, Version 2.0, <LICENSE-APACHE or
+// http://apache.org/licenses/LICENSE-2.0> or the MIT license <LICENSE-MIT or
+// http://opensource.org/licenses/MIT>, at your option. This file may not be
+// copied, modified, or distributed except according to those terms.
+
 use std::future::Future;
 
 use trust_dns_resolver::proto::error::ProtoError;
@@ -66,6 +73,13 @@ impl RuntimeProvider for AsyncStdRuntime {
     type Tcp = AsyncStdTcpStream;
     type Timer = AsyncStdTime;
     type Udp = AsyncStdUdpSocket;
+}
+
+impl AsyncStdRuntime {
+    #[cfg(test)]
+    pub(crate) fn handle(&self) -> AsyncStdRuntimeHandle {
+        AsyncStdRuntimeHandle
+    }
 }
 
 pub type AsyncStdConnection = GenericConnection;
