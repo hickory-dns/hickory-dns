fd356c258b04e4c22bce5f777a8f6c070aefa735
Benjamin Fry <benjaminfry@me.com>
2020-04-10 14:57:29 -0700

    port over all tests

---
+++ crates/async-std-resolver/src/lib.rs
@@ -0,0 +1,128 @@
+// Copyright 2015-2020 Benjamin Fry <benjaminfry@me.com>
+//
+// Licensed under the Apache License, Version 2.0, <LICENSE-APACHE or
+// http://apache.org/licenses/LICENSE-2.0> or the MIT license <LICENSE-MIT or
+// http://opensource.org/licenses/MIT>, at your option. This file may not be
+// copied, modified, or distributed except according to those terms.
+
+#![allow(clippy::needless_doctest_main)]
+
+//! The Resolver is responsible for performing recursive queries to lookup domain names.
+//!
+//! This is a 100% in process DNS resolver. It *does not* use the Host OS' resolver. If what is desired is to use the Host OS' resolver, generally in the system's libc, then the `std::net::ToSocketAddrs` variant over `&str` should be used.
+//!
+//! Unlike the `trust-dns-client`, this tries to provide a simpler interface to perform DNS queries. For update options, i.e. Dynamic DNS, the `trust-dns-client` crate must be used instead. The Resolver library is capable of searching multiple domains (this can be disabled by using an FQDN during lookup), dual-stack IPv4/IPv6 lookups, performing chained CNAME lookups, and features connection metric tracking for attempting to pick the best upstream DNS resolver.
+//!
+//! There are two types for performing DNS queries, [`Resolver`] and [`AsyncResolver`]. `Resolver` is the easiest to work with, it is a wrapper around [`AsyncResolver`]. `AsyncResolver` is a `Tokio` based async resolver, and can be used inside any `Tokio` based system.
+//!
+//! This as best as possible attempts to abide by the DNS RFCs, please file issues at https://github.com/bluejekyll/trust-dns .
+//!
+//! # Usage
+//!
+//! ## Declare dependency
+//!
+//! ```toml
+//! [dependency]
+//! async-std-resolver = "*"
+//! ```
+//!
+//! ## Using the Tokio/Async Resolver
+//!
+//! For more advanced asynchronous usage, the `AsyncResolver`] is integrated with Tokio. In fact, the [`AsyncResolver`] is used by the synchronous Resolver for all lookups.
+//!
+//! ```rust
+//! use std::net::*;
+//! use async_std::prelude::*;
+//! use async_std_resolver::{resolver, config};
+//!
+//! #[async_std::main]
+//! async fn main() {
+//!   // Construct a new Resolver with default configuration options
+//!   let resolver = resolver(
+//!     config::ResolverConfig::default(),
+//!     config::ResolverOpts::default(),
+//!   ).await.expect("failed to connect resolver");
+//!
+//!   // Lookup the IP addresses associated with a name.
+//!   // This returns a future that will lookup the IP addresses, it must be run in the Core to
+//!   //  to get the actual result.
+//!   let mut response = resolver.lookup_ip("www.example.com.").await.unwrap();
+//!
+//!   // There can be many addresses associated with the name,
+//!   //  this can return IPv4 and/or IPv6 addresses
+//!   let address = response.iter().next().expect("no addresses returned!");
+//!   if address.is_ipv4() {
+//!     assert_eq!(address, IpAddr::V4(Ipv4Addr::new(93, 184, 216, 34)));
+//!   } else {
+//!     assert_eq!(address, IpAddr::V6(Ipv6Addr::new(0x2606, 0x2800, 0x220, 0x1, 0x248, 0x1893, 0x25c8, 0x1946)));
+//!   }
+//! }
+//! ```
+//!
+//! ## Using the host system config
+//!
+//! On Unix systems, the `/etc/resolv.conf` can be used for configuration. Not all options specified in the host systems `resolv.conf` are applicable or compatible with this software. In addition there may be additional options supported which the host system does not. Example:
+//!
+//! ```
+//! use std::net::*;
+//! use async_std::prelude::*;
+//! use async_std_resolver::{resolver_from_system_conf, config};
+//!
+//! #[async_std::main]
+//! async fn main() {
+//!   // Use the host OS'es `/etc/resolv.conf`
+//!   # #[cfg(unix)]
+//!   let resolver = resolver_from_system_conf().await.unwrap();
+//!   # #[cfg(unix)]
+//!   let response = resolver.lookup_ip("www.example.com.").await.unwrap();
+//! }
+//! ```
+
+use trust_dns_resolver::AsyncResolver;
+
+use crate::runtime::AsyncStdConnection;
+use crate::runtime::AsyncStdConnectionProvider;
+use crate::runtime::AsyncStdRuntimeHandle;
+
+mod net;
+mod runtime;
+#[cfg(test)]
+mod tests;
+mod time;
+
+pub use trust_dns_resolver::config;
+pub use trust_dns_resolver::error::ResolveError;
+pub use trust_dns_resolver::lookup;
+pub use trust_dns_resolver::lookup_ip;
+pub use trust_dns_resolver::proto;
+
+/// An AsyncResolver used with async_std
+pub type AsyncStdResolver = AsyncResolver<AsyncStdConnection, AsyncStdConnectionProvider>;
+
+/// Construct a new async-std based `AsyncResolver` with the provided configuration.
+///
+/// # Arguments
+///
+/// * `config` - configuration, name_servers, etc. for the Resolver
+/// * `options` - basic lookup options for the resolver
+///
+/// # Returns
+///
+/// A tuple containing the new `AsyncResolver` and a future that drives the
+/// background task that runs resolutions for the `AsyncResolver`. See the
+/// documentation for `AsyncResolver` for more information on how to use
+/// the background future.
+pub async fn resolver(
+    config: config::ResolverConfig,
+    options: config::ResolverOpts,
+) -> Result<AsyncStdResolver, ResolveError> {
+    AsyncStdResolver::new(config, options, AsyncStdRuntimeHandle).await
+}
+
+/// Constructs a new async-std based Resolver with the system configuration.
+///
+/// This will use `/etc/resolv.conf` on Unix OSes and the registry on Windows.
+#[cfg(any(unix, target_os = "windows"))]
+pub async fn resolver_from_system_conf() -> Result<AsyncStdResolver, ResolveError> {
+    AsyncStdResolver::from_system_conf(AsyncStdRuntimeHandle).await
+}
---
+++ crates/async-std-resolver/src/tests.rs
@@ -0,0 +1,200 @@
+use trust_dns_resolver::testing;
+
+use crate::config::{ResolverConfig, ResolverOpts};
+use crate::lookup::LookupFuture;
+use crate::lookup_ip::LookupIpFuture;
+use crate::proto::xfer::DnsRequest;
+use crate::proto::Executor;
+use crate::runtime::{AsyncStdConnection, AsyncStdRuntime};
+use crate::AsyncStdResolver;
+
+fn is_send_t<T: Send>() -> bool {
+    true
+}
+
+fn is_sync_t<T: Sync>() -> bool {
+    true
+}
+
+#[test]
+fn test_send_sync() {
+    assert!(is_send_t::<ResolverConfig>());
+    assert!(is_sync_t::<ResolverConfig>());
+    assert!(is_send_t::<ResolverOpts>());
+    assert!(is_sync_t::<ResolverOpts>());
+
+    assert!(is_send_t::<AsyncStdResolver>());
+    assert!(is_sync_t::<AsyncStdResolver>());
+
+    assert!(is_send_t::<DnsRequest>());
+    assert!(is_send_t::<LookupIpFuture<AsyncStdConnection>>());
+    assert!(is_send_t::<LookupFuture<AsyncStdConnection>>());
+}
+
+#[test]
+fn test_lookup_google() {
+    use testing::lookup_test;
+    let io_loop = AsyncStdRuntime::new();
+    let handle = io_loop.handle();
+    lookup_test::<AsyncStdRuntime, AsyncStdRuntime>(ResolverConfig::google(), io_loop, handle)
+}
+
+#[test]
+fn test_lookup_cloudflare() {
+    use testing::lookup_test;
+    let io_loop = AsyncStdRuntime::new();
+    let handle = io_loop.handle();
+    lookup_test::<AsyncStdRuntime, AsyncStdRuntime>(ResolverConfig::cloudflare(), io_loop, handle)
+}
+
+#[test]
+fn test_lookup_quad9() {
+    use testing::lookup_test;
+    let io_loop = AsyncStdRuntime::new();
+    let handle = io_loop.handle();
+    lookup_test::<AsyncStdRuntime, AsyncStdRuntime>(ResolverConfig::quad9(), io_loop, handle)
+}
+
+#[test]
+fn test_ip_lookup() {
+    use testing::ip_lookup_test;
+    let io_loop = AsyncStdRuntime::new();
+    let handle = io_loop.handle();
+    ip_lookup_test::<AsyncStdRuntime, AsyncStdRuntime>(io_loop, handle)
+}
+
+#[test]
+fn test_ip_lookup_across_threads() {
+    use testing::ip_lookup_across_threads_test;
+    let io_loop = AsyncStdRuntime::new();
+    let handle = io_loop.handle();
+    ip_lookup_across_threads_test::<AsyncStdRuntime, AsyncStdRuntime>(io_loop, handle)
+}
+
+#[test]
+#[ignore] // these appear to not work on CI
+fn test_sec_lookup() {
+    use testing::sec_lookup_test;
+    let io_loop = AsyncStdRuntime::new();
+    let handle = io_loop.handle();
+    sec_lookup_test::<AsyncStdRuntime, AsyncStdRuntime>(io_loop, handle);
+}
+
+#[test]
+#[ignore] // these appear to not work on CI
+fn test_sec_lookup_fails() {
+    use testing::sec_lookup_fails_test;
+    let io_loop = AsyncStdRuntime::new();
+    let handle = io_loop.handle();
+    sec_lookup_fails_test::<AsyncStdRuntime, AsyncStdRuntime>(io_loop, handle);
+}
+
+#[test]
+#[ignore]
+#[cfg(any(unix, target_os = "windows"))]
+#[cfg(feature = "system-config")]
+fn test_system_lookup() {
+    use testing::system_lookup_test;
+    let io_loop = AsyncStdRuntime::new();
+    let handle = io_loop.handle();
+    system_lookup_test::<AsyncStdRuntime, AsyncStdRuntime>(io_loop, handle);
+}
+
+#[test]
+#[ignore]
+// these appear to not work on CI, test on macos with `10.1.0.104  a.com`
+#[cfg(unix)]
+fn test_hosts_lookup() {
+    use testing::hosts_lookup_test;
+    let io_loop = AsyncStdRuntime::new();
+    let handle = io_loop.handle();
+    hosts_lookup_test::<AsyncStdRuntime, AsyncStdRuntime>(io_loop, handle);
+}
+
+#[test]
+fn test_fqdn() {
+    use testing::fqdn_test;
+    let io_loop = AsyncStdRuntime::new();
+    let handle = io_loop.handle();
+    fqdn_test::<AsyncStdRuntime, AsyncStdRuntime>(io_loop, handle);
+}
+
+#[test]
+fn test_ndots() {
+    use testing::ndots_test;
+    let io_loop = AsyncStdRuntime::new();
+    let handle = io_loop.handle();
+    ndots_test::<AsyncStdRuntime, AsyncStdRuntime>(io_loop, handle);
+}
+
+#[test]
+fn test_large_ndots() {
+    use testing::large_ndots_test;
+    let io_loop = AsyncStdRuntime::new();
+    let handle = io_loop.handle();
+    large_ndots_test::<AsyncStdRuntime, AsyncStdRuntime>(io_loop, handle);
+}
+
+#[test]
+fn test_domain_search() {
+    use testing::domain_search_test;
+    let io_loop = AsyncStdRuntime::new();
+    let handle = io_loop.handle();
+    domain_search_test::<AsyncStdRuntime, AsyncStdRuntime>(io_loop, handle);
+}
+
+#[test]
+fn test_search_list() {
+    use testing::search_list_test;
+    let io_loop = AsyncStdRuntime::new();
+    let handle = io_loop.handle();
+    search_list_test::<AsyncStdRuntime, AsyncStdRuntime>(io_loop, handle);
+}
+
+#[test]
+fn test_idna() {
+    use testing::idna_test;
+    let io_loop = AsyncStdRuntime::new();
+    let handle = io_loop.handle();
+    idna_test::<AsyncStdRuntime, AsyncStdRuntime>(io_loop, handle);
+}
+
+#[test]
+fn test_localhost_ipv4() {
+    use testing::localhost_ipv4_test;
+    let io_loop = AsyncStdRuntime::new();
+    let handle = io_loop.handle();
+    localhost_ipv4_test::<AsyncStdRuntime, AsyncStdRuntime>(io_loop, handle);
+}
+
+#[test]
+fn test_localhost_ipv6() {
+    use testing::localhost_ipv6_test;
+    let io_loop = AsyncStdRuntime::new();
+    let handle = io_loop.handle();
+    localhost_ipv6_test::<AsyncStdRuntime, AsyncStdRuntime>(io_loop, handle);
+}
+
+#[test]
+fn test_search_ipv4_large_ndots() {
+    use testing::search_ipv4_large_ndots_test;
+    let io_loop = AsyncStdRuntime::new();
+    let handle = io_loop.handle();
+    search_ipv4_large_ndots_test::<AsyncStdRuntime, AsyncStdRuntime>(io_loop, handle);
+}
+
+#[test]
+fn test_search_ipv6_large_ndots() {
+    use testing::search_ipv6_large_ndots_test;
+    let io_loop = AsyncStdRuntime::new();
+    let handle = io_loop.handle();
+    search_ipv6_large_ndots_test::<AsyncStdRuntime, AsyncStdRuntime>(io_loop, handle);
+}
+
+#[test]
+fn test_search_ipv6_name_parse_fails() {
+    use testing::search_ipv6_name_parse_fails_test;
+    let io_loop = AsyncStdRuntime::new();
+    let handle = io_loop.handle();
+    search_ipv6_name_parse_fails_test::<AsyncStdRuntime, AsyncStdRuntime>(io_loop, handle);
+}
