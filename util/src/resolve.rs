dc0be23b8fc41fbd9d056f6dc8327da7ad1b656f
Benjamin Fry <benjaminfry@me.com>
2020-09-18 15:58:48 -0700

    add support for arbitrary nameservers to resolve.rs

--- util/src/resolve.rs
+++ util/src/resolve.rs
@@ -5,16 +5,19 @@
 // http://opensource.org/licenses/MIT>, at your option. This file may not be
 // copied, modified, or distributed except according to those terms.
 
+use std::net::SocketAddr;
+
 use console::Term;
 use structopt::StructOpt;
 
-use trust_dns_resolver::config::{NameServerConfigGroup, ResolverConfig, ResolverOpts};
+use trust_dns_resolver::config::{
+    NameServerConfig, NameServerConfigGroup, Protocol, ResolverConfig, ResolverOpts,
+};
 use trust_dns_resolver::proto::rr::RecordType;
 use trust_dns_resolver::TokioAsyncResolver;
 
 /// A CLI interface for the trust-dns-resolver.
 #[derive(Debug, StructOpt)]
-#[structopt(author = env!("CARGO_PKG_AUTHORS"))]
 struct Opts {
     /// Name to attempt to resolve, if followed by a '.' then it's a fully-qualified-domain-name.
     domainname: String,
@@ -23,17 +26,21 @@ struct Opts {
     #[structopt(short = "t", long = "type", default_value = "A")]
     ty: RecordType,
 
-    /// Use google resolvers, default, this can be combined with the other name servers
+    /// Use google resolvers, default
     #[structopt(long)]
     google: bool,
 
-    /// Use cloudflare resolvers, this can be combined with the other name servers
+    /// Use cloudflare resolvers
     #[structopt(long)]
     cloudflare: bool,
 
-    /// Use quad9 resolvers, this can be combined with the other name servers
+    /// Use quad9 resolvers
     #[structopt(long)]
     quad9: bool,
+
+    /// Specify a nameserver to use, ip and port e.g. 8.8.8.8:53 or [2001:4860:4860::8888]:53 (port required)
+    #[structopt(short = "n", long, require_delimiter = true)]
+    nameserver: Vec<SocketAddr>,
 
     /// Use ipv4 addresses only, default is both ipv4 and ipv6
     #[structopt(long)]
@@ -42,6 +49,14 @@ struct Opts {
     /// Use ipv6 addresses only, default is both ipv4 and ipv6
     #[structopt(long)]
     ipv6: bool,
+
+    /// Use only UDP, default to UDP and TCP
+    #[structopt(long)]
+    udp: bool,
+
+    /// Use only TCP, default to UDP and TCP
+    #[structopt(long)]
+    tcp: bool,
 
     /// Enable debug and all logging
     #[structopt(long)]
@@ -65,6 +80,7 @@ pub async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let opts: Opts = Opts::from_args();
     let term = Term::stdout();
 
+    // enable logging early
     let log_level = if opts.debug {
         log::LevelFilter::Debug
     } else if opts.info {
@@ -77,7 +93,7 @@ pub async fn main() -> Result<(), Box<dyn std::error::Error>> {
         log::LevelFilter::Off
     };
 
-    // enable logging early
+    // Get query term
     env_logger::builder()
         .filter_module("trust_dns_resolver", log_level)
         .filter_module("trust_dns_proto", log_level)
@@ -89,7 +105,23 @@ pub async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let name = &opts.domainname;
     let ty = opts.ty;
 
+    // Configure all the name servers
     let mut name_servers = NameServerConfigGroup::new();
+
+    for socket_addr in &opts.nameserver {
+        name_servers.push(NameServerConfig {
+            socket_addr: *socket_addr,
+            protocol: Protocol::Tcp,
+            tls_dns_name: None,
+        });
+
+        name_servers.push(NameServerConfig {
+            socket_addr: *socket_addr,
+            protocol: Protocol::Udp,
+            tls_dns_name: None,
+        });
+    }
+
     if opts.google {
         name_servers.merge(NameServerConfigGroup::google());
     }
@@ -106,8 +138,14 @@ pub async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let ipv4 = opts.ipv4 || !opts.ipv6;
     let ipv6 = opts.ipv6 || !opts.ipv4;
 
+    let udp = opts.udp || !opts.tcp;
+    let tcp = opts.tcp || !opts.udp;
+
     name_servers
         .retain(|ns| (ipv4 && ns.socket_addr.is_ipv4()) || (ipv6 && ns.socket_addr.is_ipv6()));
+    name_servers.retain(|ns| {
+        (udp && ns.protocol == Protocol::Udp) || (tcp && ns.protocol == Protocol::Tcp)
+    });
 
     let config = ResolverConfig::from_parts(None, vec![], name_servers);
 
@@ -134,7 +172,7 @@ pub async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .lookup(name.to_string(), ty, Default::default())
         .await?;
 
-    term.write_line(&format!("{:?} succeeeded, records:", lookup.query()))?;
+    term.write_line(&format!("{:?} success, records:", lookup.query()))?;
 
     for record in lookup.record_iter() {
         term.write_line(&format!("{:?}", record))?;
