66cb200c883375e09a243c9b9f0708df5c0f360f
André Cruz <acruz@cloudflare.com>
2022-04-14 13:09:59 +0100

    ignore errors when disconnected
    Don't emit warning when cancelling requests.

--- crates/proto/src/xfer/mod.rs
+++ crates/proto/src/xfer/mod.rs
@@ -44,8 +44,12 @@ pub use self::retry_dns_handle::RetryDnsHandle;
 pub use self::serial_message::SerialMessage;
 
 /// Ignores the result of a send operation and logs and ignores errors
-fn ignore_send<M, E: Debug>(result: Result<M, E>) {
+fn ignore_send<M, T>(result: Result<M, mpsc::TrySendError<T>>) {
     if let Err(error) = result {
+        if error.is_disconnected() {
+            return;
+        }
+
         warn!("error notifying wait, possible future leak: {:?}", error);
     }
 }
