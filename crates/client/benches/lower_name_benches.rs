f55a26334412907c56232ac500a5c81e0c1eb359
Jeff Hiner <37913568+jeff-hiner@users.noreply.github.com>
2022-07-20 11:06:54 -0600

    Fix bench errors in rust-analyzer

--- crates/client/benches/lower_name_benches.rs
+++ crates/client/benches/lower_name_benches.rs
@@ -5,6 +5,7 @@
 // http://opensource.org/licenses/MIT>, at your option. This file may not be
 // copied, modified, or distributed except according to those terms.
 
+#![cfg(nightly)]
 #![feature(test)]
 
 extern crate test;
