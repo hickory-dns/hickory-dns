4e1dc5d6103f7733a9b55033d0aa03629af9ccd7
Axel Viala <axel.viala@darnuria.eu>
2022-12-04 17:01:34 +0100

    Fix #1849 TTL was not updated in wildcard codepath.

--- crates/server/tests/store_file_tests.rs
+++ crates/server/tests/store_file_tests.rs
@@ -2,7 +2,7 @@ use std::str::FromStr;
 
 use trust_dns_client::rr::{LowerName, RecordType};
 use trust_dns_client::rr::{Name, RrKey};
-use trust_dns_server::authority::{ZoneType, Authority, LookupOptions};
+use trust_dns_server::authority::{Authority, LookupOptions, ZoneType};
 use trust_dns_server::store::file::{FileAuthority, FileConfig};
 
 #[macro_use]
@@ -57,7 +57,6 @@ async fn test_ttl_wilcard() {
     };
 
     let zone_name = LowerName::from_str("test.local.").unwrap();
-
     let mut authority = FileAuthority::try_from_config(
         Name::from(zone_name.clone()),
         ZoneType::Primary,
@@ -73,13 +72,16 @@ async fn test_ttl_wilcard() {
         name: LowerName::from(Name::from_ascii("simple.test.local.").unwrap()),
     };
     assert_eq!(authority.records_get_mut().get(&rrkey).unwrap().ttl(), 120);
-
     // // This one related to a wildcard don't pass arround $TTL
     let name = LowerName::from(Name::from_ascii("x.wc.test.local.").unwrap());
-    let rr = authority.lookup(&name, RecordType::A, LookupOptions::default()).await.unwrap();
-    let data = rr.into_iter()
-    .next()
-    .expect("A record not found in authority");
+    let rr = authority
+        .lookup(&name, RecordType::A, LookupOptions::default())
+        .await
+        .unwrap();
+    let data = rr
+        .into_iter()
+        .next()
+        .expect("A record not found in authority");
 
     assert_eq!(data.rr_type(), RecordType::A);
     assert_eq!(data.ttl(), 120);
