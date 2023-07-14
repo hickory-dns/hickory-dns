b5dfa011b99b079b4ee0771db814674adeeaf9f5
dAxpeDDa <daxpedda@gmail.com>
2023-07-14 19:36:23 +0200

    Proto server implementation

---
+++ crates/proto/src/h3/h3_server.rs
@@ -0,0 +1,127 @@
+// Copyright 2015-2022 Benjamin Fry <benjaminfry@me.com>
+//
+// Licensed under the Apache License, Version 2.0, <LICENSE-APACHE or
+// http://apache.org/licenses/LICENSE-2.0> or the MIT license <LICENSE-MIT or
+// http://opensource.org/licenses/MIT>, at your option. This file may not be
+// copied, modified, or distributed except according to those terms.
+
+//! HTTP/3 related server items
+
+use std::{io, net::SocketAddr, sync::Arc};
+
+use bytes::Bytes;
+use h3::server::{Connection, RequestStream};
+use h3_quinn::{BidiStream, Endpoint};
+use http::Request;
+use quinn::{EndpointConfig, ServerConfig};
+use rustls::{server::ServerConfig as TlsServerConfig, version::TLS13, Certificate, PrivateKey};
+
+use crate::{error::ProtoError, udp::UdpSocket};
+
+use super::ALPN_H3;
+
+/// A DNS-over-HTTP/3 Server, see H3ClientStream for the client counterpart
+pub struct H3Server {
+    endpoint: Endpoint,
+}
+
+impl H3Server {
+    /// Construct the new Acceptor with the associated pkcs12 data
+    pub async fn new(
+        name_server: SocketAddr,
+        cert: Vec<Certificate>,
+        key: PrivateKey,
+    ) -> Result<Self, ProtoError> {
+        // setup a new socket for the server to use
+        let socket = <tokio::net::UdpSocket as UdpSocket>::bind(name_server).await?;
+        Self::with_socket(socket, cert, key)
+    }
+
+    /// Construct the new server with an existing socket
+    pub fn with_socket(
+        socket: tokio::net::UdpSocket,
+        cert: Vec<Certificate>,
+        key: PrivateKey,
+    ) -> Result<Self, ProtoError> {
+        let mut config = TlsServerConfig::builder()
+            .with_safe_default_cipher_suites()
+            .with_safe_default_kx_groups()
+            .with_protocol_versions(&[&TLS13])
+            .expect("TLS1.3 not supported")
+            .with_no_client_auth()
+            .with_single_cert(cert, key)?;
+
+        config.alpn_protocols = vec![ALPN_H3.to_vec()];
+
+        let mut server_config = ServerConfig::with_crypto(Arc::new(config));
+        server_config.transport = Arc::new(super::transport());
+
+        let socket = socket.into_std()?;
+
+        let endpoint = Endpoint::new(
+            EndpointConfig::default(),
+            Some(server_config),
+            socket,
+            Arc::new(quinn::TokioRuntime),
+        )?;
+
+        Ok(Self { endpoint })
+    }
+
+    /// Accept the next incoming connection.
+    ///
+    /// # Returns
+    ///
+    /// A remote connection that could accept many potential requests and the remote socket address
+    pub async fn accept(&mut self) -> Result<Option<(H3Connection, SocketAddr)>, ProtoError> {
+        let connecting = match self.endpoint.accept().await {
+            Some(conn) => conn,
+            None => return Ok(None),
+        };
+
+        let remote_addr = connecting.remote_address();
+        let connection = connecting.await?;
+        Ok(Some((
+            H3Connection {
+                connection: Connection::new(h3_quinn::Connection::new(connection))
+                    .await
+                    .map_err(|e| ProtoError::from(format!("h3 connection failed: {e}")))?,
+            },
+            remote_addr,
+        )))
+    }
+
+    /// Returns the address this server is listening on
+    ///
+    /// This can be useful in tests, where a random port can be associated with the server by binding on `127.0.0.1:0` and then getting the
+    ///   associated port address with this function.
+    pub fn local_addr(&self) -> Result<SocketAddr, io::Error> {
+        self.endpoint.local_addr()
+    }
+}
+
+/// A HTTP/3 connection.
+pub struct H3Connection {
+    connection: Connection<h3_quinn::Connection, Bytes>,
+}
+
+impl H3Connection {
+    /// Accept the next request from the client
+    pub async fn accept(
+        &mut self,
+    ) -> Option<Result<(Request<()>, RequestStream<BidiStream<Bytes>, Bytes>), ProtoError>> {
+        match self.connection.accept().await {
+            Ok(Some((request, stream))) => Some(Ok((request, stream))),
+            Ok(None) => None,
+            Err(e) => Some(Err(ProtoError::from(format!("h3 request failed: {e}")))),
+        }
+    }
+
+    /// Shutdown the connection.
+    pub async fn shutdown(&mut self) -> Result<(), ProtoError> {
+        self.connection
+            .shutdown(0)
+            .await
+            .map_err(|e| ProtoError::from(format!("h3 connection shutdown failed: {e}")))
+    }
+}
