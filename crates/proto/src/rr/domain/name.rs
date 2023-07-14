698c5dd03db228351d8deb8041beded92a3f18dc
dAxpeDDa <daxpedda@gmail.com>
2023-07-14 19:30:11 +0200

    Fix Clippy

--- crates/proto/src/rr/domain/name.rs
+++ crates/proto/src/rr/domain/name.rs
@@ -1777,7 +1777,7 @@ mod tests {
         for i in 0..10000 {
             let name = Name::from_ascii(format!("name{i}.example.com.")).unwrap();
             result = name.emit(&mut encoder);
-            if let Err(..) = result {
+            if result.is_err() {
                 break;
             }
         }
