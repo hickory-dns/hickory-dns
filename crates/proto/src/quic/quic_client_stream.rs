3f9a68c15e76dc05739de22390eb58ec8ba43b16
Zhang Jingqiang <zh_jq@outlook.com>
2023-08-02 09:22:56 +0800

    use new version of webpki

--- crates/proto/src/quic/quic_client_stream.rs
+++ crates/proto/src/quic/quic_client_stream.rs
@@ -273,7 +273,7 @@ impl QuicClientStreamBuilder {
 pub fn client_config_tls13_webpki_roots() -> TlsClientConfig {
     use rustls::{OwnedTrustAnchor, RootCertStore};
     let mut root_store = RootCertStore::empty();
-    root_store.add_server_trust_anchors(webpki_roots::TLS_SERVER_ROOTS.0.iter().map(|ta| {
+    root_store.add_server_trust_anchors(webpki_roots::TLS_SERVER_ROOTS.iter().map(|ta| {
         OwnedTrustAnchor::from_subject_spki_name_constraints(
             ta.subject,
             ta.spki,
--- util/src/dns.rs
+++ util/src/dns.rs
@@ -479,7 +479,7 @@ fn record_set_from(
 #[cfg(feature = "dns-over-rustls")]
 fn tls_config() -> ClientConfig {
     let mut root_store = RootCertStore::empty();
-    root_store.add_server_trust_anchors(webpki_roots::TLS_SERVER_ROOTS.0.iter().map(|ta| {
+    root_store.add_server_trust_anchors(webpki_roots::TLS_SERVER_ROOTS.iter().map(|ta| {
         OwnedTrustAnchor::from_subject_spki_name_constraints(
             ta.subject,
             ta.spki,
