a5bc0b872850f6b06f7d996754039f838f12390a
Benjamin Fry <benjaminfry@me.com>
2023-09-06 09:02:45 -0700

    add run-example target to justfile to simply start trust-dns for manual testing

--- scripts/run_with_example.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-trust_dns_dir=$(dirname $0)/..
-crates_dir=${trust_dns_dir:?}/crates
-
-cargo run -- -d -c ${crates_dir:?}/src/config/test/example.toml -z ${trust_dns_dir}/src/config/test -p 2053
