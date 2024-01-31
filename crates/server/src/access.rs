414abf70870a78804b3c5670c8abc38e54f429c4
Benjamin Fry <benjaminfry@me.com>
2024-01-31 09:16:13 -0800

    remove single add access control methods, only allow slice

--- crates/server/src/access.rs
+++ crates/server/src/access.rs
@@ -19,38 +19,30 @@ pub(crate) struct AccessControl {
 
 impl AccessControl {
     /// Insert a new network that is denied access to the server
-    pub(crate) fn insert_deny(&mut self, network: IpNet) {
-        match network {
-            IpNet::V4(v4) => {
-                self.ipv4.deny.insert(v4);
-            }
-            IpNet::V6(v6) => {
-                self.ipv6.deny.insert(v6);
+    pub(crate) fn insert_deny(&mut self, networks: &[IpNet]) {
+        for network in networks {
+            match network {
+                IpNet::V4(v4) => {
+                    self.ipv4.deny.insert(*v4);
+                }
+                IpNet::V6(v6) => {
+                    self.ipv6.deny.insert(*v6);
+                }
             }
         }
     }
 
     /// Insert a new network that is allowed access to the server
-    pub(crate) fn insert_allow(&mut self, network: IpNet) {
-        match network {
-            IpNet::V4(v4) => {
-                self.ipv4.allow.insert(v4);
+    pub(crate) fn insert_allow(&mut self, networks: &[IpNet]) {
+        for network in networks {
+            match network {
+                IpNet::V4(v4) => {
+                    self.ipv4.allow.insert(*v4);
+                }
+                IpNet::V6(v6) => {
+                    self.ipv6.allow.insert(*v6);
+                }
             }
-            IpNet::V6(v6) => {
-                self.ipv6.allow.insert(v6);
-            }
-        }
-    }
-
-    pub(crate) fn insert_deny_all(&mut self, networks: &[IpNet]) {
-        for net in networks {
-            self.insert_deny(*net);
-        }
-    }
-
-    pub(crate) fn insert_allow_all(&mut self, networks: &[IpNet]) {
-        for net in networks {
-            self.insert_allow(*net);
         }
     }
 
@@ -138,7 +130,7 @@ mod tests {
     #[test]
     fn test_v4() {
         let mut access = AccessControl::default();
-        access.insert_allow("192.168.1.0/24".parse().unwrap());
+        access.insert_allow(&["192.168.1.0/24".parse().unwrap()]);
 
         assert!(access.allow("192.168.1.1".parse().unwrap()).is_ok());
         assert!(access.allow("192.168.1.255".parse().unwrap()).is_ok());
@@ -149,7 +141,7 @@ mod tests {
     #[test]
     fn test_v6() {
         let mut access = AccessControl::default();
-        access.insert_allow("fd00::/120".parse().unwrap());
+        access.insert_allow(&["fd00::/120".parse().unwrap()]);
 
         assert!(access.allow("fd00::1".parse().unwrap()).is_ok());
         assert!(access.allow("fd00::00ff".parse().unwrap()).is_ok());
@@ -160,7 +152,7 @@ mod tests {
     #[test]
     fn test_deny_v4() {
         let mut access = AccessControl::default();
-        access.insert_deny("192.168.1.0/24".parse().unwrap());
+        access.insert_deny(&["192.168.1.0/24".parse().unwrap()]);
 
         assert!(access.allow("192.168.1.1".parse().unwrap()).is_err());
         assert!(access.allow("192.168.1.255".parse().unwrap()).is_err());
@@ -171,7 +163,7 @@ mod tests {
     #[test]
     fn test_deny_v6() {
         let mut access = AccessControl::default();
-        access.insert_deny("fd00::/120".parse().unwrap());
+        access.insert_deny(&["fd00::/120".parse().unwrap()]);
 
         assert!(access.allow("fd00::1".parse().unwrap()).is_err());
         assert!(access.allow("fd00::00ff".parse().unwrap()).is_err());
@@ -182,8 +174,8 @@ mod tests {
     #[test]
     fn test_deny_allow_v4() {
         let mut access = AccessControl::default();
-        access.insert_deny("192.168.0.0/16".parse().unwrap());
-        access.insert_allow("192.168.1.0/24".parse().unwrap());
+        access.insert_deny(&["192.168.0.0/16".parse().unwrap()]);
+        access.insert_allow(&["192.168.1.0/24".parse().unwrap()]);
 
         assert!(access.allow("192.168.1.1".parse().unwrap()).is_ok());
         assert!(access.allow("192.168.1.255".parse().unwrap()).is_ok());
