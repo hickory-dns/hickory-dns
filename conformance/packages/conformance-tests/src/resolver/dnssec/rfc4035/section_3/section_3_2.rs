a797cd93cc853303e6a531448484db827f70ab44
Jorge Aparicio <jorge.aparicio@ferrous-systems.com>
2024-06-17 13:33:06 +0200

    test DO=1 query followed by DO=0 query

--- conformance/packages/conformance-tests/src/resolver/dnssec/rfc4035/section_3/section_3_2.rs
+++ conformance/packages/conformance-tests/src/resolver/dnssec/rfc4035/section_3/section_3_2.rs
@@ -52,6 +52,37 @@ fn do_bit_not_set_in_request() -> Result<()> {
     Ok(())
 }
 
+// this ensures that even in the presence of a cached (answer+rrsig) we strip the dnssec records as
+// per the RFC
+#[test]
+#[ignore]
+fn on_do_0_query_strips_dnssec_records_even_if_it_cached_a_previous_do_1_query() -> Result<()> {
+    let network = &Network::new()?;
+    let ns = NameServer::new(&dns_test::PEER, FQDN::ROOT, network)?
+        .sign()?
+        .start()?;
+    let resolver = Resolver::new(network, ns.root_hint()).start(&dns_test::SUBJECT)?;
+
+    let client = Client::new(network)?;
+    let settings = *DigSettings::default().dnssec().recurse();
+    let resolver_addr = resolver.ipv4_addr();
+    let ans = client.dig(settings, resolver_addr, RecordType::SOA, &FQDN::ROOT)?;
+
+    let [answer, rrsig] = ans.answer.try_into().unwrap();
+
+    assert!(matches!(answer, Record::SOA(_)));
+    assert!(matches!(rrsig, Record::RRSIG(_)));
+
+    let settings = *DigSettings::default().recurse();
+    let ans = client.dig(settings, resolver_addr, RecordType::SOA, &FQDN::ROOT)?;
+
+    let [answer] = ans.answer.try_into().unwrap();
+
+    assert!(matches!(answer, Record::SOA(_)));
+
+    Ok(())
+}
+
 #[test]
 fn if_do_bit_not_set_in_request_then_requested_dnssec_record_is_not_stripped() -> Result<()> {
     let network = &Network::new()?;
