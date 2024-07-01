03ccc9ac0447c4578693025942ec378db0fe70eb
Jorge Aparicio <jorge.aparicio@ferrous-systems.com>
2024-07-01 12:59:26 +0200

    dns-test: make unit tests use the checked out version of this repo
    instead of https://github.com/hickory-dns/hickory-dns
    
    the GitHub URL won't include the changes being tested in CI and can make
    the tests fail due to it being an older version

--- conformance/packages/dns-test/src/resolver.rs
+++ conformance/packages/dns-test/src/resolver.rs
@@ -209,9 +209,8 @@ mod tests {
     fn terminate_hickory_works() -> Result<()> {
         let network = Network::new()?;
         let ns = NameServer::new(&Implementation::Unbound, FQDN::ROOT, &network)?.start()?;
-        let resolver = Resolver::new(&network, ns.root_hint()).start(&Implementation::Hickory(
-            Repository("https://github.com/hickory-dns/hickory-dns"),
-        ))?;
+        let resolver = Resolver::new(&network, ns.root_hint())
+            .start(&Implementation::Hickory(Repository(crate::repo_root())))?;
         let logs = resolver.terminate()?;
 
         // Hickory-DNS start sequence log has been consumed in `ResolverSettings.start`.
