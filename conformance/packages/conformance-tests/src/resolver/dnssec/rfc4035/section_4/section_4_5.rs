6568398ea36366e536f29691fb1b42c5d175a10f
Sebastian Ziebell <sebastian.ziebell@ferrous-systems.com>
2024-07-04 12:06:48 +0200

    Add test to check cache behavior
    As given in RFC 4035 section 4.5.

--- conformance/packages/conformance-tests/src/resolver/dnssec/rfc4035/section_4/section_4_5.rs
+++ conformance/packages/conformance-tests/src/resolver/dnssec/rfc4035/section_4/section_4_5.rs
@@ -6,6 +6,7 @@ use dns_test::{
     Network, Resolver, Result, FQDN,
 };
 
+/// Two queries are sent with DNSSEC enabled, the second query should take the answer from the cache.
 #[test]
 fn caches_dnssec_records() -> Result<()> {
     let network = &Network::new()?;
@@ -32,6 +33,43 @@ fn caches_dnssec_records() -> Result<()> {
     }
 
     let mut tshark = tshark.unwrap();
+    tshark.wait_for_capture()?;
+
+    let captures = tshark.terminate()?;
+
+    // second query is cached so no communication between the resolver and the nameserver is
+    // expected
+    let ns_addr = ns.ipv4_addr();
+    for Capture { direction, .. } in captures {
+        assert_ne!(ns_addr, direction.peer_addr());
+    }
+
+    Ok(())
+}
+
+/// Two queries are sent, the first without DNSSEC enabled is put into the cache, the second query with
+/// DNSSEC enabled will fetch its result from the cache
+#[test]
+fn caches_query_without_dnssec_to_return_all_dnssec_records_in_subsequent_query() -> Result<()> {
+    let network = &Network::new()?;
+    let ns = NameServer::new(&dns_test::PEER, FQDN::ROOT, network)?
+        .sign()?
+        .start()?;
+    let resolver = Resolver::new(network, ns.root_hint()).start(&dns_test::SUBJECT)?;
+
+    let client = Client::new(network)?;
+
+    // send first query without DNSSEC, fills cache
+    let settings = *DigSettings::default().recurse();
+    let dig = client.dig(settings, resolver.ipv4_addr(), RecordType::SOA, &FQDN::ROOT)?;
+    assert!(dig.status.is_noerror());
+
+    // send second query to fetch all DNSSEC records
+    let mut tshark = resolver.eavesdrop()?;
+    let settings = *DigSettings::default().dnssec().recurse();
+    let dig = client.dig(settings, resolver.ipv4_addr(), RecordType::SOA, &FQDN::ROOT)?;
+    assert!(dig.status.is_noerror());
+
     tshark.wait_for_capture()?;
 
     let captures = tshark.terminate()?;
