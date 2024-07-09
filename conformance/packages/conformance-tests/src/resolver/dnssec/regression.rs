85df66a60cf7261431289adc04c812307573bedd
Jorge Aparicio <jorge.aparicio@ferrous-systems.com>
2024-07-09 17:54:04 +0200

    conformance: add regression test for #2285

--- conformance/packages/conformance-tests/src/resolver/dnssec/regression.rs
+++ conformance/packages/conformance-tests/src/resolver/dnssec/regression.rs
@@ -50,3 +50,48 @@ fn includes_rrsig_record_in_ns_query() -> Result<()> {
 
     Ok(())
 }
+
+/// This is a regression test for https://github.com/hickory-dns/hickory-dns/issues/2285
+#[test]
+#[ignore]
+fn can_validate_ns_query() -> Result<()> {
+    let network = Network::new()?;
+    let leaf_ns = NameServer::new(&dns_test::PEER, FQDN::NAMESERVERS, &network)?;
+
+    let Graph {
+        nameservers: _nameservers,
+        root,
+        trust_anchor,
+    } = Graph::build(
+        leaf_ns,
+        Sign::Yes {
+            settings: SignSettings::default(),
+        },
+    )?;
+
+    // NOTE this is a security-aware, *validating* resolver
+    let resolver = Resolver::new(&network, root)
+        .trust_anchor(&trust_anchor.unwrap())
+        .start()?;
+
+    let resolver_addr = resolver.ipv4_addr();
+
+    let client = Client::new(resolver.network())?;
+
+    let output = client.dig(
+        *DigSettings::default().authentic_data().recurse(),
+        resolver_addr,
+        RecordType::NS,
+        &FQDN::NAMESERVERS,
+    )?;
+
+    // bug: this returned SERVFAIL instead of NOERROR with AD=1
+    assert!(output.status.is_noerror());
+    assert!(output.flags.authenticated_data);
+
+    // check that the record type is what we expect
+    let [ns] = output.answer.try_into().unwrap();
+    assert!(matches!(ns, Record::NS(_)));
+
+    Ok(())
+}
