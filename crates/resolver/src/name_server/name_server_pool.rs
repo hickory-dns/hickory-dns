5b2950100f498b9e035f08d31a98efe0da1922ec
Laurent Rouquette <lrouquet@adobe.com>
2024-07-31 16:11:43 -0700

    fmt

--- crates/resolver/src/name_server/name_server_pool.rs
+++ crates/resolver/src/name_server/name_server_pool.rs
@@ -218,9 +218,17 @@ where
                         debug!("truncated response received, retrying over TCP");
                         Ok(response)
                     }
-                    Err(e) if matches!(e.kind(), ProtoErrorKind::NoRecordsFound{ response_code: ResponseCode::NXDomain, .. }) => {
+                    Err(e)
+                        if matches!(
+                            e.kind(),
+                            ProtoErrorKind::NoRecordsFound {
+                                response_code: ResponseCode::NXDomain,
+                                ..
+                            }
+                        ) =>
+                    {
                         debug!("NoRecordsFound with NXDomain, not retrying over TCP");
-                        return Err(e)
+                        return Err(e);
                     }
                     Err(e) if opts.try_tcp_on_error || e.is_no_connections() || e.is_io() => {
                         debug!("error from UDP, retrying over TCP: {}", e);
