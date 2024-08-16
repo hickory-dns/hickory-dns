fe1190865285b0aa8c3f3050057d555491cf6d43
Sebastian Ziebell <sebastian.ziebell@ferrous-systems.com>
2024-08-16 15:26:19 +0200

    Rewrite test

--- conformance/packages/conformance-tests/src/name_server/rfc8914.rs
+++ conformance/packages/conformance-tests/src/name_server/rfc8914.rs
@@ -1,46 +1,37 @@
-use std::net::Ipv4Addr;
+use std::time::{Duration, SystemTime};
 
 use dns_test::client::{Client, DigSettings, ExtendedDnsError};
 use dns_test::name_server::NameServer;
-use dns_test::record::RecordType;
+use dns_test::record::{Record, RecordType};
 use dns_test::zone_file::SignSettings;
-use dns_test::{Network, Result, FQDN};
+use dns_test::{Network, Resolver, Result, FQDN};
+
+const ONE_DAY: Duration = Duration::from_secs(60 * 60 * 24);
 
 #[test]
 #[ignore]
 fn ede_not_authoritative() -> Result<()> {
-    // TODO remove these
-    let fqdns = (0u8..=32)
-        .map(|label| {
-            FQDN(format!(
-                "{0}abcdefghijklmnopqrstuvwxyz.nameservers.com.",
-                label
-            ))
-        })
-        .collect::<Result<Vec<_>>>()?;
+    let settings = SignSettings::default()
+        .inception(SystemTime::now() - 30 * ONE_DAY)
+        .expiration(SystemTime::now() - 10 * ONE_DAY);
 
-    // main name server, add referals to other (non-existent) nameservers
-    let origin_fqdn = FQDN::NAMESERVERS;
-    let network = Network::new()?;
-    let mut ns = NameServer::new(&dns_test::SUBJECT, origin_fqdn.clone(), &network)?;
-    fqdns.iter().for_each(|fqdn| {
-        let ip = Ipv4Addr::new(1, 2, 3, 4);
-        ns.referral(origin_fqdn.clone(), fqdn.clone(), ip);
-    });
+    let network = &Network::new()?;
+    let mut ns = NameServer::new(&dns_test::PEER, FQDN::ROOT, network)?;
+    ns.add(Record::txt(FQDN::COM, "Something here"));
+    let ns = ns.sign(settings)?.start()?;
 
-    let ns = ns.sign(SignSettings::default())?.start()?;
+    let resolver = Resolver::new(network, ns.root_hint())
+        .trust_anchor(ns.trust_anchor().expect("Failed to get trust anchor"))
+        .extended_dns_errors()
+        .start()?;
 
-    let client = Client::new(&network)?;
+    let client = Client::new(network)?;
+    let settings = *DigSettings::default().dnssec();
 
-    // fetch ALL records for zone '.'
-    let answer = client.dig(
-        *DigSettings::default().dnssec(),
-        ns.ipv4_addr(),
-        RecordType::SOA,
-        &FQDN::ROOT,
-    )?;
+    let dig = client.dig(settings, resolver.ipv4_addr(), RecordType::TXT, &FQDN::COM)?;
 
-    assert_eq!(Some(ExtendedDnsError::NotAuhoritative), answer.ede);
+    assert!(dig.status.is_refused());
+    assert_eq!(Some(ExtendedDnsError::NotAuthoritative), dig.ede);
 
     Ok(())
 }
