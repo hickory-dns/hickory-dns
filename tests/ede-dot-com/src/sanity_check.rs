df38ce6a8839b1c670d01cbf0d2ca42fd3b4308c
Jorge Aparicio <jorge.aparicio@ferrous-systems.com>
2024-08-26 17:56:15 +0200

    add ede-dot-com test suite

---
+++ tests/ede-dot-com/src/sanity_check.rs
@@ -0,0 +1,27 @@
+use std::env;
+
+use dns_test::Result;
+
+// control subdomain; authentic data is expected
+#[test]
+fn valid() -> Result<()> {
+    let response = crate::compare("valid")?;
+
+    dbg!(&response);
+
+    assert!(response.status.is_noerror());
+    assert!(response.flags.authenticated_data);
+
+    Ok(())
+}
+
+#[test]
+fn dns_test_vars_are_not_set() {
+    for var in ["DNS_TEST_SUBJECT", "DNS_TEST_PEER"] {
+        assert!(
+            env::var_os(var).is_none(),
+            "the environment variable {} must not be set when running this test suite",
+            var
+        )
+    }
+}
