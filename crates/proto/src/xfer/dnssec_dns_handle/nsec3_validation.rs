69ef531cac99ebb811a29944b7aa1275d94cd210
Andrei Listochkin <andrei.listochkin@ferrous-systems.com>
2024-08-28 18:54:30 +0100

    module docs explaining NSEC3 terminology

--- crates/proto/src/xfer/dnssec_dns_handle/nsec3_validation.rs
+++ crates/proto/src/xfer/dnssec_dns_handle/nsec3_validation.rs
@@ -1,3 +1,81 @@
+//! Throughout this module several NSEC3 specific terms are used.
+//!
+//! "closest_encloser" - a name that is a longest parent / grandparent /
+//!     great-grandparent, etc. of a `query_name`, that DOES exist.
+//!
+//! "next_closer" - a name that is one level deeper in the hierarchy from
+//!     `closest_encloser`, that DOES NOT exist in the zone.
+//!
+//! "wildcard of closest encloser" - a name in a form of `*.closest_encloser`.
+//!     If it exists then `query_name` would be serviced by the wildcard.
+//!
+//! "covering NSEC3 record" - NSEC3 record name has a hash (`hashed_owner_name`),
+//!     and inside the record data there's `next_hashed_owner_name`.
+//!     If the hash of `query_name` fits between the two hashes then the record
+//!     "covers" `query_name`
+//!
+//! "matching NSEC3 record" - exists for *existing* names only.
+//!     The `hashed_owner_name` would match one of the names *exactly*.
+//!
+//! In general:
+//! * if a name exists we would expect to see its "matching" NSEC3 record,
+//! * if the name doesn't exist we would expect to see a "covering" NSEC3 record.
+//!
+//! A various combinations of matching and covering records for `closest_encloser`,
+//! `next_closer`, and `wildcard_encloser` can tell us whether `query_name`
+//! exists, whether `wildcard_encloser` exists, and whether its own or
+//! `wildcard_encloser`'s records have a record of a requested type.
+//!
+//! NOTE: A single NSEC3 record can in theory address multiple situations.
+//! Thus, the number of records is not important as long as all
+//! conditions are represented by them.
+//!
+//! NOTE: Normally, when there exist a wildcard that a given name fits into,
+//! it is said that the wildcard "covers" the name. But since the wording
+//! around NSEC3 records uses "cover" in a specific sense we use the word
+//! "service" instead:
+//! `*.w.soa.name` *services* `x.w.soa.name`
+//!
+//! In general, avoid using the word "cover" for anything not related to
+//! "covering NSEC3 record"
+//!
+//! ## Examples
+//!
+//! To explain them let's use the following example zone:
+//!
+//! soa.name
+//! *.w.soa.name
+//! c.e.soa.name
+//!
+//! if we request:
+//!     `query_name` = `x.y.z.nc.c.e.soa.name`
+//! then:
+//!     `closest_encloser` == `c.e.soa.name` (the first existing ancestor)
+//!     `next_closer`      == `nc.c.e.soa.name` (doesn't exist in the zone)
+//!     `wildcard_encloser` == `*.c.e.soa.name`
+//!
+//! if we request:
+//!     `query_name` = `x.y.z.nc.w.soa.name`
+//! then:
+//!     `closest_encloser` == `w.soa.name`
+//!     `next_closer`      == `nc.w.soa.name`
+//!     `wildcard_encloser` == `*.w.soa.name`
+//!
+//! if we request:
+//!     `query_name` = `x.soa.name`
+//! then:
+//!     `closest_encloser` == `soa.name`
+//!     `next_closer`      == `x.soa.name`
+//!     `wildcard_encloser` == `*.soa.name`
+//!
+//! if we request:
+//!     `query_name` = `e.soa.name`
+//! then:
+//!     `closest_encloser` == `e.soa.name`
+//!     `next_closer`      == doesn't exist
+//!     `wildcard_encloser` == `*.soa.name`
+//!
+
 use crate::{
     op::{Query, ResponseCode},
     rr::{
