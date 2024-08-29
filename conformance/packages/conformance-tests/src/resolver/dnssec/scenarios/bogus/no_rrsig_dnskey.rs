b84b0f33b13ff2b0916392b02cc988396df44da6
Jorge Aparicio <jorge.aparicio@ferrous-systems.com>
2024-08-29 15:56:40 +0200

    conformance: test a different query

--- conformance/packages/conformance-tests/src/resolver/dnssec/scenarios/bogus/no_rrsig_dnskey.rs
+++ conformance/packages/conformance-tests/src/resolver/dnssec/scenarios/bogus/no_rrsig_dnskey.rs
@@ -1,11 +1,13 @@
 //! the RRSIGs that cover the DNSKEY have been removed
 
+use std::net::Ipv4Addr;
+
 use dns_test::{
     client::{Client, DigSettings, ExtendedDnsError},
     name_server::{Graph, NameServer, Sign},
     record::{Record, RecordType},
     zone_file::SignSettings,
-    Network, Resolver, Result, FQDN,
+    Implementation, Network, Resolver, Result, FQDN,
 };
 
 #[test]
@@ -59,6 +61,82 @@ fn query_dnskey_record() -> Result<()> {
         RecordType::DNSKEY,
         &leaf_zone,
     )?;
+
+    dbg!(&output);
+
+    assert!(output.status.is_servfail());
+
+    if dns_test::SUBJECT.is_unbound() {
+        // check that this failed for the right reason
+        assert_eq!(Some(ExtendedDnsError::RrsigsMissing), output.ede);
+    }
+
+    Ok(())
+}
+
+#[test]
+#[ignore]
+fn query_other_record() -> Result<()> {
+    let network = Network::new()?;
+
+    let leaf_zone = FQDN::TEST_TLD.push_label("no-rrsig-dnskey");
+
+    // other implementations fail the PRE-CONDITION below
+    let peer = Implementation::Bind;
+    let mut leaf_ns = NameServer::new(&peer, leaf_zone.clone(), &network)?;
+    leaf_ns.add(Record::a(leaf_zone.clone(), Ipv4Addr::new(1, 2, 3, 4)));
+    let leaf_ns_addr = leaf_ns.ipv4_addr();
+
+    let Graph {
+        nameservers: _nameservers,
+        root,
+        trust_anchor,
+    } = Graph::build(
+        leaf_ns,
+        Sign::AndAmend {
+            settings: SignSettings::default(),
+            mutate: &|zone, records| {
+                if *zone == leaf_zone {
+                    let mut remove_count = 0;
+                    for index in (0..records.len()).rev() {
+                        if let Record::RRSIG(rrsig) = &records[index] {
+                            if rrsig.type_covered == RecordType::DNSKEY {
+                                records.swap_remove(index);
+                                remove_count += 1;
+                            }
+                        }
+                    }
+                    // sanity check
+                    assert_ne!(0, remove_count);
+                }
+            },
+        },
+    )?;
+
+    let trust_anchor = trust_anchor.unwrap();
+    let mut resolver = Resolver::new(&network, root);
+
+    if dns_test::SUBJECT.is_unbound() {
+        resolver.extended_dns_errors();
+    }
+
+    let resolver = resolver.trust_anchor(&trust_anchor).start()?;
+
+    let client = Client::new(&network)?;
+    // PRE-CONDITION the authoritative server must include the RRSIG records
+    let settings = *DigSettings::default().dnssec();
+    let output = client.dig(settings, leaf_ns_addr, RecordType::A, &leaf_zone)?;
+    assert!(output.status.is_noerror());
+    assert!(
+        output
+            .answer
+            .iter()
+            .any(|record| matches!(record, Record::RRSIG(_))),
+        "peer name server fails PRE-CONDITION"
+    );
+
+    let settings = *DigSettings::default().recurse().authentic_data();
+    let output = client.dig(settings, resolver.ipv4_addr(), RecordType::A, &leaf_zone)?;
 
     dbg!(&output);
 
