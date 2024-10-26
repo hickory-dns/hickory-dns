8c02db5d28c233627170d0d390da193384a54616
Marcus Butler <marcusb@marcusb.org>
2024-10-26 15:10:29 -0500

    test for cname loop

--- tests/e2e-tests/src/recursor/cname/scenarios.rs
+++ tests/e2e-tests/src/recursor/cname/scenarios.rs
@@ -17,6 +17,7 @@
 ///  www10.example2.testing IN CNAME www11.example.testing.
 ///  www12.example2.testing IN CNAME www13.example.testing.
 ///
+use std::fs;
 use std::net::Ipv4Addr;
 use std::thread;
 use std::time::Duration;
@@ -162,6 +163,58 @@ fn multi_level_cname_tests() -> Result<()> {
     } else {
         panic!("Error");
     }
+
+    Ok(())
+}
+
+/// ensure that no more than MAX_CNAME_LOOKUPS will be resolved.
+#[test]
+fn cname_lookup_limit_test() -> Result<()> {
+    let target_fqdn = FQDN("host.testing.")?;
+
+    let network = Network::new()?;
+
+    let mut root_ns = NameServer::new(&Implementation::test_peer(), FQDN::ROOT, &network)?;
+    let leaf_ns = NameServer::new(&Implementation::Dnslib, FQDN::TEST_TLD, &network)?;
+
+    let script = fs::read_to_string("src/recursor/cname/cname_loop.py")?;
+
+    leaf_ns.cp("/script.py", &script[..])?;
+
+    root_ns.referral(
+        FQDN::TEST_TLD,
+        FQDN("primary.tld-server.testing.")?,
+        leaf_ns.ipv4_addr(),
+    );
+
+    let root_hint: Root = root_ns.root_hint();
+
+    let resolver =
+        Resolver::new(&network, root_hint).start_with_subject(&Implementation::hickory())?;
+
+    let client = Client::new(resolver.network())?;
+
+    let _root_ns = root_ns.start()?;
+    let _leaf_ns = leaf_ns.start()?;
+
+    thread::sleep(Duration::from_secs(2));
+    let a_settings = *DigSettings::default().recurse().authentic_data();
+    let res = client.dig(
+        a_settings,
+        resolver.ipv4_addr(),
+        RecordType::A,
+        &target_fqdn,
+    );
+
+    match res {
+        Ok(res) => {
+            assert!(res.status.is_servfail());
+            assert_eq!(res.answer.len(), 0);
+        }
+        Err(e) => panic!("error {e:?}; resolver logs: {}", resolver.logs().unwrap()),
+    }
+
+    assert!(resolver.logs().unwrap().contains("cname limit exceeded"));
 
     Ok(())
 }
