fe21a25179ac7c8413c896de565d7fe0a4eb3fe6
Christian Poveda <christian.poveda@ferrous-systems.com>
2024-10-31 16:02:02 -0500

    Unignore passing tests

--- conformance/packages/conformance-tests/src/resolver/nsec.rs
+++ conformance/packages/conformance-tests/src/resolver/nsec.rs
@@ -11,7 +11,6 @@ use dns_test::{
 };
 
 #[test]
-#[ignore]
 fn zone_exist_domain_does_not_nsec3() -> Result<()> {
     zone_exist_domain_does_not(Nsec::_3 { salt: None })
 }
@@ -22,7 +21,6 @@ fn zone_exist_domain_does_not_nsec() -> Result<()> {
 }
 
 #[test]
-#[ignore]
 fn zone_does_not_exist_nsec3() -> Result<()> {
     zone_does_not_exist(Nsec::_3 { salt: None })
 }
