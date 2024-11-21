426dc2408703bf1607f9bc81896767446b85eba4
David Cook <dcook@letsencrypt.org>
2024-11-21 13:06:36 -0600

    Update default logging filter to match all crates

--- bin/src/hickory-dns.rs
+++ bin/src/hickory-dns.rs
@@ -848,7 +848,7 @@ fn get_env() -> String {
 
 fn all_hickory_dns(level: impl ToString) -> String {
     format!(
-        "hickory_dns={level},hickory_server={level},{env}",
+        "hickory_={level},{env}",
         level = level.to_string().to_lowercase(),
         env = get_env()
     )
