75c7abdc092020beb70247a0049d993678e7d700
Dirkjan Ochtman <dirkjan@ochtman.nl>
2024-11-22 15:26:42 +0100

    proto: move key_tag() logic close to usage site

--- crates/proto/src/dnssec/rdata/ds.rs
+++ crates/proto/src/dnssec/rdata/ds.rs
@@ -12,6 +12,7 @@ use std::fmt::{self, Display, Formatter};
 #[cfg(feature = "serde")]
 use serde::{Deserialize, Serialize};
 
+use super::{DNSSECRData, DnsSecError};
 use crate::{
     dnssec::{rdata::DNSKEY, Algorithm, DigestType, PublicKey},
     error::{ProtoError, ProtoResult},
@@ -20,8 +21,6 @@ use crate::{
         BinDecodable, BinDecoder, BinEncodable, BinEncoder, Restrict, RestrictedMath,
     },
 };
-
-use super::{DNSSECRData, DnsSecError};
 
 /// [RFC 4034, DNSSEC Resource Records, March 2005](https://tools.ietf.org/html/rfc4034#section-5)
 ///
@@ -90,7 +89,7 @@ impl DS {
         algorithm: Algorithm,
         digest_type: DigestType,
     ) -> Result<Self, DnsSecError> {
-        let tag = public_key.key_tag();
+        let tag = key_tag(public_key.public_bytes());
         let dnskey = DNSKEY::from_key(public_key, algorithm);
         Ok(Self::new(
             tag,
@@ -325,6 +324,74 @@ impl Display for DS {
             digest = data_encoding::HEXUPPER_PERMISSIVE.encode(&self.digest)
         )
     }
+}
+
+/// The key tag is calculated as a hash to more quickly lookup a DNSKEY.
+///
+/// [RFC 1035](https://tools.ietf.org/html/rfc1035), DOMAIN NAMES - IMPLEMENTATION AND SPECIFICATION, November 1987
+///
+/// ```text
+/// RFC 2535                DNS Security Extensions               March 1999
+///
+/// 4.1.6 Key Tag Field
+///
+///  The "key Tag" is a two octet quantity that is used to efficiently
+///  select between multiple keys which may be applicable and thus check
+///  that a public key about to be used for the computationally expensive
+///  effort to check the signature is possibly valid.  For algorithm 1
+///  (MD5/RSA) as defined in [RFC 2537], it is the next to the bottom two
+///  octets of the public key modulus needed to decode the signature
+///  field.  That is to say, the most significant 16 of the least
+///  significant 24 bits of the modulus in network (big endian) order. For
+///  all other algorithms, including private algorithms, it is calculated
+///  as a simple checksum of the KEY RR as described in Appendix C.
+///
+/// Appendix C: Key Tag Calculation
+///
+///  The key tag field in the SIG RR is just a means of more efficiently
+///  selecting the correct KEY RR to use when there is more than one KEY
+///  RR candidate available, for example, in verifying a signature.  It is
+///  possible for more than one candidate key to have the same tag, in
+///  which case each must be tried until one works or all fail.  The
+///  following reference implementation of how to calculate the Key Tag,
+///  for all algorithms other than algorithm 1, is in ANSI C.  It is coded
+///  for clarity, not efficiency.  (See section 4.1.6 for how to determine
+///  the Key Tag of an algorithm 1 key.)
+///
+///  /* assumes int is at least 16 bits
+///     first byte of the key tag is the most significant byte of return
+///     value
+///     second byte of the key tag is the least significant byte of
+///     return value
+///     */
+///
+///  int keytag (
+///
+///          unsigned char key[],  /* the RDATA part of the KEY RR */
+///          unsigned int keysize, /* the RDLENGTH */
+///          )
+///  {
+///  long int    ac;    /* assumed to be 32 bits or larger */
+///
+///  for ( ac = 0, i = 0; i < keysize; ++i )
+///      ac += (i&1) ? key[i] : key[i]<<8;
+///  ac += (ac>>16) & 0xFFFF;
+///  return ac & 0xFFFF;
+///  }
+/// ```
+fn key_tag(public_key: &[u8]) -> u16 {
+    let mut ac = 0;
+
+    for (i, k) in public_key.iter().enumerate() {
+        ac += if i & 0x0001 == 0x0001 {
+            *k as usize
+        } else {
+            (*k as usize) << 8
+        };
+    }
+
+    ac += (ac >> 16) & 0xFFFF;
+    (ac & 0xFFFF) as u16 // this is unnecessary, no?
 }
 
 #[cfg(test)]
