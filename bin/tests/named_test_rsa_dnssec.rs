7c476012cb3a58b85f36c4d893e44a40e6f93766
David Cook <dcook@letsencrypt.org>
2025-02-07 14:41:20 -0600

    Skip requests with DAU option in tests

--- bin/tests/named_test_rsa_dnssec.rs
+++ bin/tests/named_test_rsa_dnssec.rs
@@ -88,10 +88,6 @@ fn generic_test(config_toml: &str, key_path: &str, key_format: KeyFormat, algori
         let client = standard_tcp_conn(tcp_port.expect("no tcp port"));
         let (client, bg) = io_loop.block_on(client);
         hickory_proto::spawn_bg(&io_loop, bg);
-        query_all_dnssec_with_rfc6975(&mut io_loop, client, algorithm);
-        let client = standard_tcp_conn(tcp_port.expect("no tcp port"));
-        let (client, bg) = io_loop.block_on(client);
-        hickory_proto::spawn_bg(&io_loop, bg);
         query_all_dnssec_wo_rfc6975(&mut io_loop, client, algorithm);
 
         // test that request with Dnssec client is successful, i.e. validates chain
