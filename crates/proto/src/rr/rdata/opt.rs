domenukk/trust-dns

Move uses of std to core and alloc, introduce initial no_std with extern crate std

crates/proto/src/rr/rdata/opt.rs
@@ -8,13 +8,13 @@
 //! option record for passing protocol options between the client and server
 #![allow(clippy::use_self)]
 
-use std::fmt;
+use alloc::str::FromStr;
+use alloc::vec::Vec;
+use core::fmt;
 use std::net::{IpAddr, Ipv4Addr, Ipv6Addr};
-use std::str::FromStr;
 
 #[cfg(feature = "serde")]
 use serde::{Deserialize, Serialize};
-
 use tracing::warn;
 
 use crate::{
@@ -781,6 +781,8 @@ impl FromStr for ClientSubnet {
 #[cfg(test)]
 mod tests {
     #![allow(clippy::dbg_macro, clippy::print_stdout)]
+
+    use std::println;
 
     use super::*;
 

crates/proto/src/serialize/txt/rdata_parsers/soa.rs
@@ -16,6 +16,8 @@
 
 //! Parser for SOA text form
 
+use alloc::string::ToString;
+
 use crate::{
     rr::{domain::Name, rdata::SOA},
     serialize::txt::{
@@ -77,7 +79,7 @@ pub(crate) fn parse<'i, I: Iterator<Item = &'i str>>(
 
 #[test]
 fn test_parse() {
-    use std::str::FromStr;
+    use alloc::str::FromStr;
 
     let soa_tokens = vec![
         "hickory-dns.org.",

hickory-dns/hickory-dns

crates/proto/src/rr/rdata/opt.rs
@@ -8,13 +8,13 @@
 //! option record for passing protocol options between the client and server
 #![allow(clippy::use_self)]
 
-use std::fmt;
+use alloc::vec::Vec;
+use core::fmt;
+use core::str::FromStr;
 use std::net::{IpAddr, Ipv4Addr, Ipv6Addr};
-use std::str::FromStr;
 
 #[cfg(feature = "serde")]
 use serde::{Deserialize, Serialize};
-
 use tracing::warn;
 
 use crate::{
@@ -781,6 +781,8 @@ impl FromStr for ClientSubnet {
 #[cfg(test)]
 mod tests {
     #![allow(clippy::dbg_macro, clippy::print_stdout)]
+
+    use std::println;
 
     use super::*;
 

crates/proto/src/serialize/txt/rdata_parsers/soa.rs
@@ -16,6 +16,8 @@
 
 //! Parser for SOA text form
 
+use alloc::string::ToString;
+
 use crate::{
     rr::{domain::Name, rdata::SOA},
     serialize::txt::{
@@ -77,7 +79,7 @@ pub(crate) fn parse<'i, I: Iterator<Item = &'i str>>(
 
 #[test]
 fn test_parse() {
-    use std::str::FromStr;
+    use core::str::FromStr;
 
     let soa_tokens = vec![
         "hickory-dns.org.",