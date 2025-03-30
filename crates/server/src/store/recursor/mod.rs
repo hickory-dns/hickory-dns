e7b6c577cd0f87120741c6be7baef4d54d233b77
Dirkjan Ochtman <dirkjan@ochtman.nl>
2025-03-30 13:16:46 +0200

    server: fold recursor config module into parent

--- crates/server/src/store/recursor/mod.rs
+++ crates/server/src/store/recursor/mod.rs
@@ -9,32 +9,44 @@
 
 //! Recursive resolver related types
 
-use std::{io, path::Path, time::Instant};
+#[cfg(feature = "__dnssec")]
+use std::sync::Arc;
+use std::{
+    borrow::Cow,
+    collections::HashSet,
+    fs::File,
+    io::{self, Read},
+    net::SocketAddr,
+    path::{Path, PathBuf},
+    time::Instant,
+};
 
+use ipnet::IpNet;
+use serde::Deserialize;
 use tracing::{debug, info};
 
 #[cfg(feature = "__dnssec")]
-use crate::{authority::Nsec3QueryInfo, dnssec::NxProofKind};
+use crate::{authority::Nsec3QueryInfo, dnssec::NxProofKind, proto::dnssec::TrustAnchors};
 use crate::{
     authority::{
         Authority, LookupControlFlow, LookupError, LookupObject, LookupOptions, MessageRequest,
         UpdateResult, ZoneType,
     },
+    error::ConfigError,
     proto::{
         op::{Query, ResponseCode},
-        rr::{LowerName, Name, Record, RecordType},
+        rr::{LowerName, Name, RData, Record, RecordSet, RecordType},
+        serialize::txt::{ParseError, Parser},
         xfer::Protocol,
     },
-    recursor::Recursor,
+    recursor::{DnssecPolicy, Recursor},
     resolver::{
         config::{NameServerConfig, NameServerConfigGroup},
+        dns_lru::TtlConfig,
         lookup::Lookup,
     },
     server::RequestInfo,
 };
-
-mod config;
-pub use self::config::RecursiveConfig;
 
 /// An authority that performs recursive resolutions.
 ///
@@ -224,5 +236,188 @@ impl LookupObject for RecursiveLookup {
 
     fn take_additionals(&mut self) -> Option<Box<dyn LookupObject>> {
         None
+    }
+}
+
+/// Configuration for file based zones
+#[derive(Clone, Deserialize, Eq, PartialEq, Debug)]
+#[serde(deny_unknown_fields)]
+pub struct RecursiveConfig {
+    /// File with roots, aka hints
+    pub roots: PathBuf,
+
+    /// Maximum nameserver cache size
+    pub ns_cache_size: Option<usize>,
+
+    /// Maximum DNS record cache size
+    pub record_cache_size: Option<usize>,
+
+    /// Maximum recursion depth for queries. Set to 0 for unlimited recursion depth.
+    #[serde(default = "recursion_limit_default")]
+    pub recursion_limit: u8,
+
+    /// Maximum recursion depth for building NS pools. Set to 0 for unlimited recursion depth.
+    #[serde(default = "ns_recursion_limit_default")]
+    pub ns_recursion_limit: u8,
+
+    /// DNSSEC policy
+    #[serde(default)]
+    pub dnssec_policy: DnssecPolicyConfig,
+
+    /// Networks that will be queried during resolution
+    #[serde(default)]
+    pub allow_server: Vec<IpNet>,
+
+    /// Networks that will not be queried during resolution
+    #[serde(default)]
+    pub deny_server: Vec<IpNet>,
+
+    /// Local UDP ports to avoid when making outgoing queries
+    #[serde(default)]
+    pub avoid_local_udp_ports: HashSet<u16>,
+
+    /// Caching policy, setting minimum and maximum TTLs
+    #[serde(default)]
+    pub cache_policy: TtlConfig,
+
+    /// Enable case randomization.
+    ///
+    /// Randomize the case of letters in query names, and require that responses preserve the case
+    /// of the query name, in order to mitigate spoofing attacks. This is only applied over UDP.
+    ///
+    /// This implements the mechanism described in
+    /// [draft-vixie-dnsext-dns0x20-00](https://datatracker.ietf.org/doc/html/draft-vixie-dnsext-dns0x20-00).
+    #[serde(default)]
+    pub case_randomization: bool,
+}
+
+impl RecursiveConfig {
+    pub(crate) fn read_roots(
+        &self,
+        root_dir: Option<&Path>,
+    ) -> Result<Vec<SocketAddr>, ConfigError> {
+        let path = if let Some(root_dir) = root_dir {
+            Cow::Owned(root_dir.join(&self.roots))
+        } else {
+            Cow::Borrowed(&self.roots)
+        };
+
+        let mut roots = File::open(path.as_ref())?;
+        let mut roots_str = String::new();
+        roots.read_to_string(&mut roots_str)?;
+
+        let (_zone, roots_zone) =
+            Parser::new(roots_str, Some(path.into_owned()), Some(Name::root())).parse()?;
+
+        // TODO: we may want to deny some of the root nameservers, for reasons...
+        Ok(roots_zone
+            .values()
+            .flat_map(RecordSet::records_without_rrsigs)
+            .map(Record::data)
+            .filter_map(RData::ip_addr) // we only want IPs
+            .map(|ip| SocketAddr::from((ip, 53))) // all the roots only have tradition DNS ports
+            .collect())
+    }
+}
+
+fn recursion_limit_default() -> u8 {
+    12
+}
+
+fn ns_recursion_limit_default() -> u8 {
+    16
+}
+
+/// DNSSEC policy configuration
+#[derive(Clone, Debug, Default, Deserialize, Eq, PartialEq)]
+#[serde(deny_unknown_fields)]
+#[allow(missing_copy_implementations)]
+pub enum DnssecPolicyConfig {
+    /// security unaware; DNSSEC records will not be requested nor processed
+    #[default]
+    SecurityUnaware,
+
+    /// DNSSEC validation is disabled; DNSSEC records will be requested and processed
+    #[cfg(feature = "__dnssec")]
+    ValidationDisabled,
+
+    /// DNSSEC validation is enabled and will use the chosen `trust_anchor` set of keys
+    #[cfg(feature = "__dnssec")]
+    ValidateWithStaticKey {
+        /// set to `None` to use built-in trust anchor
+        path: Option<PathBuf>,
+    },
+}
+
+impl DnssecPolicyConfig {
+    pub(crate) fn load(&self) -> Result<DnssecPolicy, ParseError> {
+        Ok(match self {
+            Self::SecurityUnaware => DnssecPolicy::SecurityUnaware,
+            #[cfg(feature = "__dnssec")]
+            Self::ValidationDisabled => DnssecPolicy::ValidationDisabled,
+            #[cfg(feature = "__dnssec")]
+            Self::ValidateWithStaticKey { path } => DnssecPolicy::ValidateWithStaticKey {
+                trust_anchor: path
+                    .as_ref()
+                    .map(|path| TrustAnchors::from_file(path))
+                    .transpose()?
+                    .map(Arc::new),
+            },
+        })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[cfg(all(feature = "__dnssec", feature = "toml"))]
+    use super::*;
+
+    #[cfg(all(feature = "__dnssec", feature = "toml"))]
+    #[test]
+    fn can_parse_recursive_config() {
+        let input = r#"roots = "/etc/root.hints"
+dnssec_policy.ValidateWithStaticKey.path = "/etc/trusted-key.key""#;
+
+        let config: RecursiveConfig = toml::from_str(input).unwrap();
+
+        if let DnssecPolicyConfig::ValidateWithStaticKey { path } = config.dnssec_policy {
+            assert_eq!(Some(Path::new("/etc/trusted-key.key")), path.as_deref());
+        } else {
+            unreachable!()
+        }
+    }
+
+    #[cfg(all(feature = "recursor", feature = "toml"))]
+    #[test]
+    fn can_parse_recursor_cache_policy() {
+        use std::time::Duration;
+
+        use hickory_proto::rr::RecordType;
+
+        let input = r#"roots = "/etc/root.hints"
+
+[cache_policy.default]
+positive_max_ttl = 14400
+
+[cache_policy.A]
+positive_max_ttl = 3600"#;
+
+        let config: RecursiveConfig = toml::from_str(input).unwrap();
+
+        assert_eq!(
+            *config
+                .cache_policy
+                .positive_response_ttl_bounds(RecordType::MX)
+                .end(),
+            Duration::from_secs(14400)
+        );
+
+        assert_eq!(
+            *config
+                .cache_policy
+                .positive_response_ttl_bounds(RecordType::A)
+                .end(),
+            Duration::from_secs(3600)
+        )
     }
 }
