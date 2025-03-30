0b5336537bf9f0fbd93f6eb437f142e14efc0075
Dirkjan Ochtman <dirkjan@ochtman.nl>
2025-03-30 12:54:39 +0200

    server: merge error module into authority

--- crates/server/src/authority/mod.rs
+++ crates/server/src/authority/mod.rs
@@ -7,17 +7,24 @@
 
 //! Module for `Catalog` of `Authority` zones which are responsible for storing `RRSet` records.
 
-use crate::proto::op::ResponseCode;
+use std::{io, sync::Arc};
 
-/// Result of an Update operation
-pub type UpdateResult<T> = Result<T, ResponseCode>;
+use enum_as_inner::EnumAsInner;
+use thiserror::Error;
+
+use crate::proto::op::ResponseCode;
+use crate::proto::rr::{Record, rdata::SOA};
+use crate::proto::{ProtoError, ProtoErrorKind};
+#[cfg(feature = "recursor")]
+use crate::recursor::ErrorKind;
+#[cfg(feature = "resolver")]
+use crate::resolver::ResolveError;
 
 mod auth_lookup;
 #[allow(clippy::module_inception)]
 mod authority;
 pub(crate) mod authority_object;
 mod catalog;
-mod error;
 pub(crate) mod message_request;
 mod message_response;
 mod zone_type;
@@ -26,12 +33,127 @@ pub use self::auth_lookup::{
     AnyRecords, AuthLookup, AuthLookupIter, LookupRecords, LookupRecordsIter,
 };
 pub use self::authority::{Authority, LookupControlFlow, LookupOptions};
+#[cfg(feature = "__dnssec")]
+pub use self::authority::{DnssecAuthority, Nsec3QueryInfo};
 pub use self::authority_object::{AuthorityObject, DnssecSummary, EmptyLookup, LookupObject};
 pub use self::catalog::Catalog;
-pub use self::error::LookupError;
 pub use self::message_request::{MessageRequest, Queries, UpdateRequest};
 pub use self::message_response::{MessageResponse, MessageResponseBuilder};
 pub use self::zone_type::ZoneType;
 
-#[cfg(feature = "__dnssec")]
-pub use self::authority::{DnssecAuthority, Nsec3QueryInfo};
+/// Result of an Update operation
+pub type UpdateResult<T> = Result<T, ResponseCode>;
+
+// TODO: should this implement Failure?
+#[allow(clippy::large_enum_variant)]
+/// A query could not be fulfilled
+#[derive(Debug, EnumAsInner, Error)]
+#[non_exhaustive]
+pub enum LookupError {
+    /// A record at the same Name as the query exists, but not of the queried RecordType
+    #[error("The name exists, but not for the record requested")]
+    NameExists,
+    /// There was an error performing the lookup
+    #[error("Error performing lookup: {0}")]
+    ResponseCode(ResponseCode),
+    /// Proto error
+    #[error("Proto error: {0}")]
+    ProtoError(#[from] ProtoError),
+    /// Resolve Error
+    #[cfg(feature = "resolver")]
+    #[error("Forward resolution error: {0}")]
+    ResolveError(#[from] ResolveError),
+    /// Recursive Resolver Error
+    #[cfg(feature = "recursor")]
+    #[error("Recursive resolution error: {0}")]
+    RecursiveError(#[from] hickory_recursor::Error),
+    /// An underlying IO error occurred
+    #[error("io error: {0}")]
+    Io(io::Error),
+}
+
+impl LookupError {
+    /// Create a lookup error, specifying that a name exists at the location, but no matching RecordType
+    pub fn for_name_exists() -> Self {
+        Self::NameExists
+    }
+
+    /// This is a non-existent domain name
+    pub fn is_nx_domain(&self) -> bool {
+        match self {
+            Self::ResponseCode(ResponseCode::NXDomain) => true,
+            #[cfg(feature = "resolver")]
+            Self::ResolveError(e) if e.is_nx_domain() => true,
+            #[cfg(feature = "recursor")]
+            Self::RecursiveError(e) if e.is_nx_domain() => true,
+            _ => false,
+        }
+    }
+
+    /// Returns true if no records were returned
+    pub fn is_no_records_found(&self) -> bool {
+        match self {
+            #[cfg(feature = "resolver")]
+            Self::ResolveError(e) if e.is_no_records_found() => true,
+            #[cfg(feature = "recursor")]
+            Self::RecursiveError(e) if e.is_no_records_found() => true,
+            _ => false,
+        }
+    }
+
+    /// Returns the SOA record, if the error contains one
+    pub fn into_soa(self) -> Option<Box<Record<SOA>>> {
+        match self {
+            #[cfg(feature = "resolver")]
+            Self::ResolveError(e) => e.into_soa(),
+            #[cfg(feature = "recursor")]
+            Self::RecursiveError(e) => e.into_soa(),
+            _ => None,
+        }
+    }
+
+    /// Return authority records
+    pub fn authorities(&self) -> Option<Arc<[Record]>> {
+        match self {
+            Self::ProtoError(e) => match e.kind() {
+                ProtoErrorKind::NoRecordsFound { authorities, .. } => authorities.clone(),
+                _ => None,
+            },
+            #[cfg(feature = "recursor")]
+            Self::RecursiveError(e) => match e.kind() {
+                ErrorKind::Forward(fwd) => fwd.authorities.clone(),
+                ErrorKind::Proto(proto) => match proto.kind() {
+                    ProtoErrorKind::NoRecordsFound { authorities, .. } => authorities.clone(),
+                    _ => None,
+                },
+                _ => None,
+            },
+            _ => None,
+        }
+    }
+
+    /// This is a non-existent domain name
+    pub fn is_refused(&self) -> bool {
+        matches!(*self, Self::ResponseCode(ResponseCode::Refused))
+    }
+}
+
+impl From<ResponseCode> for LookupError {
+    fn from(code: ResponseCode) -> Self {
+        // this should never be a NoError
+        debug_assert!(code != ResponseCode::NoError);
+        Self::ResponseCode(code)
+    }
+}
+
+impl From<io::Error> for LookupError {
+    fn from(e: io::Error) -> Self {
+        Self::Io(e)
+    }
+}
+
+impl From<LookupError> for io::Error {
+    fn from(e: LookupError) -> Self {
+        Self::new(io::ErrorKind::Other, Box::new(e))
+    }
+}
