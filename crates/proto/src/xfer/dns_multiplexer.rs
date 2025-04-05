83e603190c34afab4036bd36f6ce4761bc7d0bdd
Aden-Q <qzc0512@gmail.com>
2025-04-05 15:24:31 -0500

    docs: order for arg document

--- crates/proto/src/xfer/dns_multiplexer.rs
+++ crates/proto/src/xfer/dns_multiplexer.rs
@@ -137,9 +137,9 @@ where
     ///
     /// * `stream` - A stream of bytes that can be used to send/receive DNS messages
     ///   (see TcpClientStream or UdpClientStream)
+    /// * `stream_handle` - The handle for the `stream` on which bytes can be sent/received.
     /// * `timeout_duration` - All requests may fail due to lack of response, this is the time to
     ///   wait for a response before canceling the request.
-    /// * `stream_handle` - The handle for the `stream` on which bytes can be sent/received.
     /// * `signer` - An optional signer for requests, needed for Updates with Sig0, otherwise not needed
     pub fn with_timeout<F>(
         stream: F,
