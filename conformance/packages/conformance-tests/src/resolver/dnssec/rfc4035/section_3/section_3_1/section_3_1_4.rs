82ca37343faf23927575f2401af5534b81df66d2
David Cook <dcook@letsencrypt.org>
2025-04-10 16:19:39 -0500

    Reduce rightward drift when destructuring capture

--- conformance/packages/conformance-tests/src/resolver/dnssec/rfc4035/section_3/section_3_1/section_3_1_4.rs
+++ conformance/packages/conformance-tests/src/resolver/dnssec/rfc4035/section_3/section_3_1/section_3_1_4.rs
@@ -52,27 +52,27 @@ fn on_clients_ds_query_it_queries_the_parent_zone() -> Result<()> {
     tshark.wait_until(
         |captures| {
             captures.iter().any(|capture| {
-                if let Capture {
+                let Capture {
                     direction: Direction::Outgoing { destination },
                     message,
                 } = capture
-                {
-                    if *destination != client_addr {
-                        let queries = message.as_value()["Queries"]
-                            .as_object()
-                            .expect("expected Object");
-                        println!("outgoing query: {queries:?}");
-                        for query in queries.keys() {
-                            if query.contains("type DS") {
-                                // the domain name in the query omits the last `.` so strip it from
-                                // FQDN::TEST_DOMAIN
-                                let test_domain = FQDN::TEST_DOMAIN.as_str();
-                                let test_domain =
-                                    test_domain.strip_suffix('.').unwrap_or(test_domain);
-                                assert!(query.contains(test_domain));
-                                assert_eq!(tld_ns_addr, *destination);
-                                return true;
-                            }
+                else {
+                    return false;
+                };
+                if *destination != client_addr {
+                    let queries = message.as_value()["Queries"]
+                        .as_object()
+                        .expect("expected Object");
+                    println!("outgoing query: {queries:?}");
+                    for query in queries.keys() {
+                        if query.contains("type DS") {
+                            // the domain name in the query omits the last `.` so strip it from
+                            // FQDN::TEST_DOMAIN
+                            let test_domain = FQDN::TEST_DOMAIN.as_str();
+                            let test_domain = test_domain.strip_suffix('.').unwrap_or(test_domain);
+                            assert!(query.contains(test_domain));
+                            assert_eq!(tld_ns_addr, *destination);
+                            return true;
                         }
                     }
                 }
