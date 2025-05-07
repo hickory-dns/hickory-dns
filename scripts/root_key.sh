5d9bb7aa1322a34012a2844cdade686b3954db40
Daniel McCarney <daniel@binaryparadox.net>
2025-05-07 16:28:27 -0400

    proj: remove .csr file from root, gen script
    The `Kjqmt7v.csr` file in the root of the project seems to only be
    referenced by `scripts/root_key.sh`, which in turn looks to output
    a file to a directory that no longer exists.
    
    On the assumption neither are needed anymore, let's ax 'em and tidy up
    the project root a smidge.

--- scripts/root_key.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-hickory_dns_dir=$(dirname $0)/..
-
-openssl req -text -noout -verify -in ${hickory_dns_dir}/Kjqmt7v.csr -inform DER -pubkey -out ${hickory_dns_dir}crates/proto/src/rr/dnssec/Kjqmt7v.pem
