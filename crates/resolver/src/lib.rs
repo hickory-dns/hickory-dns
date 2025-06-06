289af2954ae1d9a8b2fd1e0a2aabb1c5eb0274c7
Dirkjan Ochtman <dirkjan@ochtman.nl>
2025-06-06 15:32:20 +0200

    resolver: inline small h2 module

--- crates/resolver/src/lib.rs
+++ crates/resolver/src/lib.rs
@@ -199,8 +199,6 @@ pub use proto::rr::{IntoName, Name};
 pub mod caching_client;
 pub mod config;
 pub mod dns_lru;
-#[cfg(feature = "__https")]
-mod h2;
 #[cfg(feature = "__h3")]
 mod h3;
 mod hosts;
