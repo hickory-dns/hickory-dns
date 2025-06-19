2cc23c6541fd9b910b25d73c780f8f2eb85cf027
Daniel McCarney <daniel@binaryparadox.net>
2025-06-19 16:43:52 -0400

    server/server: fix TimeoutStream::new comment
    It doesn't take a reactor arg.

--- crates/server/src/server/timeout_stream.rs
+++ crates/server/src/server/timeout_stream.rs
@@ -25,7 +25,6 @@ impl<S> TimeoutStream<S> {
     ///
     /// * `stream` - stream to wrap
     /// * `timeout_duration` - timeout between each request, once exceed the connection is killed
-    /// * `reactor_handle` - reactor used for registering new timeouts
     pub fn new(stream: S, timeout_duration: Duration) -> Self {
         Self {
             stream,
