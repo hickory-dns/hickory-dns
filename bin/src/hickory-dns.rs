d6050187e2770440e00c189bf760f9a386543bfa
Daniel McCarney <daniel@binaryparadox.net>
2025-06-19 15:59:32 -0400

    hickory-server: update usage opts rustdoc comment
    Catches the module-level rustdoc comment up with the current state of
    the command-line options.

--- bin/src/hickory-dns.rs
+++ bin/src/hickory-dns.rs
@@ -12,14 +12,17 @@
 //!       hickory-dns (-h | --help | --version)
 //!
 //! Options:
-//!    -q, --quiet             Disable INFO messages, WARN and ERROR will remain
-//!    -d, --debug             Turn on DEBUG messages (default is only INFO)
-//!    -h, --help              Show this message
-//!    -v, --version           Show the version of hickory-dns
-//!    -c FILE, --config=FILE  Path to configuration file, default is /etc/named.toml
-//!    -z DIR, --zonedir=DIR   Path to the root directory for all zone files, see also config toml
-//!    -p PORT, --port=PORT    Override the listening port
-//!    --tls-port=PORT         Override the listening port for TLS connections
+//!       --validate           Test validation of configuration files
+//!       --workers <WORKERS>  Number of runtime workers, defaults to the number of CPU cores
+//!       -q, --quiet              Disable INFO messages, WARN and ERROR will remain
+//!       -d, --debug              Turn on `DEBUG` messages (default is only `INFO`)
+//!       -c, --config <NAME>      Path to configuration file of named server [default: /etc/named.toml]
+//!       -z, --zonedir <DIR>      Path to the root directory for all zone files, see also config toml
+//!       -p, --port <PORT>        Listening port for DNS queries, overrides any value in config file
+//!           --disable-tcp        Disable TCP protocol, overrides any value in config file
+//!           --disable-udp        Disable UDP protocol, overrides any value in config file
+//!       -h, --help               Print help
+//!       -V, --version            Print version
 //! ```
 
 #![recursion_limit = "128"]
