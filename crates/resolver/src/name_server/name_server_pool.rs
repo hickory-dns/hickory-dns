47991dae4628fe29ffdcde0be86bb3d2056c672f
David Cook <dcook@letsencrypt.org>
2025-07-18 11:03:10 -0500

    Replace uses of FutureExt::map with async blocks

--- crates/resolver/src/name_server/name_server_pool.rs
+++ crates/resolver/src/name_server/name_server_pool.rs
@@ -14,7 +14,6 @@ use std::sync::{
 };
 use std::time::Duration;
 
-use futures_util::future::FutureExt;
 use futures_util::stream::{FuturesUnordered, Stream, StreamExt, once};
 use hickory_proto::NoRecords;
 use hickory_proto::op::ResponseCode;
@@ -188,10 +187,9 @@ impl<P: ConnectionProvider> PoolState<P> {
 
             let mut requests = par_conns
                 .into_iter()
-                .map(|conn| {
-                    conn.send(request.clone())
-                        .first_answer()
-                        .map(|result| result.map_err(|e| (conn, e)))
+                .map(|conn| async {
+                    let result = conn.send(request.clone()).first_answer().await;
+                    result.map_err(|e| (conn, e))
                 })
                 .collect::<FuturesUnordered<_>>();
 
