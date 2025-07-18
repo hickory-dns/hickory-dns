8a22fa08f27decec86b99c7125afbcf829b5e147
Dirkjan Ochtman <dirkjan@ochtman.nl>
2025-07-18 14:46:41 +0200

    proto: remove deprecated DnsRequestOptions::expects_multiple_responses

--- crates/proto/src/xfer/dns_request.rs
+++ crates/proto/src/xfer/dns_request.rs
@@ -15,11 +15,6 @@ use crate::op::{Message, Query};
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
 #[non_exhaustive]
 pub struct DnsRequestOptions {
-    /// When true, the underlying DNS protocols will not return on the first response received.
-    ///
-    /// Setting this option will cause the underlying protocol to await the timeout, and then return all Responses.
-    #[deprecated]
-    pub expects_multiple_responses: bool,
     // /// If set, then the request will terminate early if all types have been received
     // pub expected_record_types: Option<SmallVec<[RecordType; 2]>>,
     // TODO: add EDNS options here?
@@ -38,10 +33,8 @@ pub struct DnsRequestOptions {
 
 impl Default for DnsRequestOptions {
     fn default() -> Self {
-        #[allow(deprecated)]
         Self {
             max_request_depth: 26,
-            expects_multiple_responses: false,
             use_edns: false,
             edns_set_dnssec_ok: false,
             recursion_desired: true,
