9e95492923a1668fdcee492116f3b851d595eea2
David Cook <dcook@letsencrypt.org>
2025-07-25 09:22:41 -0500

    Add runtime generic parameter to SqliteAuthority

--- crates/server/src/store/sqlite/mod.rs
+++ crates/server/src/store/sqlite/mod.rs
@@ -9,6 +9,7 @@
 
 #[cfg(feature = "__dnssec")]
 use std::fs;
+use std::marker::PhantomData;
 #[cfg(feature = "__dnssec")]
 use std::str::FromStr;
 #[cfg(feature = "__dnssec")]
@@ -36,6 +37,7 @@ use crate::{
         op::ResponseCode,
         op::message::ResponseSigner,
         rr::{DNSClass, LowerName, Name, RData, Record, RecordSet, RecordType, RrKey},
+        runtime::{RuntimeProvider, TokioRuntimeProvider},
     },
     server::{Request, RequestInfo},
     store::{
@@ -47,15 +49,17 @@ use crate::{
 use crate::{
     authority::{DnssecAuthority, Nsec3QueryInfo, UpdateRequest},
     dnssec::NxProofKind,
-    proto::dnssec::{
-        DnsSecResult, SigSigner, TSigResponseContext, TSigner, Verifier,
-        rdata::{
-            DNSSECRData,
-            key::KEY,
-            tsig::{TsigAlgorithm, TsigError},
+    proto::{
+        dnssec::{
+            DnsSecResult, SigSigner, TSigResponseContext, TSigner, Verifier,
+            rdata::{
+                DNSSECRData,
+                key::KEY,
+                tsig::{TsigAlgorithm, TsigError},
+            },
         },
+        op::MessageSignature,
     },
-    proto::op::MessageSignature,
 };
 #[cfg(feature = "__dnssec")]
 use LookupControlFlow::Continue;
@@ -68,7 +72,7 @@ pub use persistence::Journal;
 /// Authorities default to DNSClass IN. The ZoneType specifies if this should be treated as the
 /// start of authority for the zone, is a Secondary, or a cached zone.
 #[allow(dead_code)]
-pub struct SqliteAuthority {
+pub struct SqliteAuthority<P = TokioRuntimeProvider> {
     in_memory: InMemoryAuthority,
     journal: Mutex<Option<Journal>>,
     axfr_policy: AxfrPolicy,
@@ -78,9 +82,10 @@ pub struct SqliteAuthority {
     metrics: PersistentStoreMetrics,
     #[cfg(feature = "__dnssec")]
     tsig_signers: Vec<TSigner>,
+    _phantom: PhantomData<P>,
 }
 
-impl SqliteAuthority {
+impl<P: RuntimeProvider + Send + Sync> SqliteAuthority<P> {
     /// Creates a new Authority.
     ///
     /// # Arguments
@@ -110,6 +115,7 @@ impl SqliteAuthority {
             metrics: PersistentStoreMetrics::new("sqlite"),
             #[cfg(feature = "__dnssec")]
             tsig_signers: Vec::new(),
+            _phantom: PhantomData,
         }
     }
 
@@ -1007,7 +1013,7 @@ impl SqliteAuthority {
     }
 }
 
-impl Deref for SqliteAuthority {
+impl<P> Deref for SqliteAuthority<P> {
     type Target = InMemoryAuthority;
 
     fn deref(&self) -> &Self::Target {
@@ -1015,14 +1021,14 @@ impl Deref for SqliteAuthority {
     }
 }
 
-impl DerefMut for SqliteAuthority {
+impl<P> DerefMut for SqliteAuthority<P> {
     fn deref_mut(&mut self) -> &mut Self::Target {
         &mut self.in_memory
     }
 }
 
 #[async_trait::async_trait]
-impl Authority for SqliteAuthority {
+impl<P: RuntimeProvider + Send + Sync> Authority for SqliteAuthority<P> {
     /// What type is this zone
     fn zone_type(&self) -> ZoneType {
         self.in_memory.zone_type()
@@ -1197,7 +1203,7 @@ impl Authority for SqliteAuthority {
 
 #[cfg(feature = "__dnssec")]
 #[async_trait::async_trait]
-impl DnssecAuthority for SqliteAuthority {
+impl<P: RuntimeProvider + Send + Sync> DnssecAuthority for SqliteAuthority<P> {
     async fn add_update_auth_key(&self, name: Name, key: KEY) -> DnsSecResult<()> {
         self.in_memory.add_update_auth_key(name, key).await
     }
