f2aaf43d5827d00db1f802834dbd1ed7317278e7
David Cook <dcook@letsencrypt.org>
2025-08-08 16:19:56 -0500

    Simplify objects in build_forwarded_response()

--- crates/server/src/authority/catalog.rs
+++ crates/server/src/authority/catalog.rs
@@ -840,15 +840,12 @@ async fn build_forwarded_response(
 
                         match record.record_type() {
                             RecordType::SOA => None,
-                            _ => Some(Arc::new(RecordSet::from(record.clone()))),
+                            _ => Some(record.clone()),
                         }
                     })
                     .collect();
 
-                AuthLookup::answers(
-                    LookupRecords::many(LookupOptions::default(), authorities),
-                    None,
-                )
+                AuthLookup::answers(LookupRecords::Section(authorities), None)
             } else {
                 AuthLookup::default()
             };
@@ -964,14 +961,14 @@ async fn build_forwarded_response(
             .filter_map(|record| {
                 let record_type = record.record_type();
                 if record_type == query.query_type() || !record_type.is_dnssec() {
-                    Some(Arc::new(RecordSet::from(record.clone())))
+                    Some(record.clone())
                 } else {
                     None
                 }
             })
             .collect();
 
-        AuthLookup::answers(LookupRecords::many(LookupOptions::default(), auth), None)
+        AuthLookup::answers(LookupRecords::Section(auth), None)
     } else {
         authorities
     };
