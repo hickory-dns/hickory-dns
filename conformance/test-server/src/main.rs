eab32216d8afe98839c18b34358d811883871864
Marcus Butler <marcusb@marcusb.org>
2025-09-03 17:47:31 -0500

    migrate bad txid test to TestServer

--- conformance/test-server/src/main.rs
+++ conformance/test-server/src/main.rs
@@ -21,6 +21,7 @@ async fn main() -> Result<()> {
     let transport = args.transport;
     let handler = match &args.handler[..] {
         "base" => handlers::base_handler,
+        "bad_txid" => handlers::bad_txid_handler,
         _ => {
             return Err(anyhow::Error::msg("unknown handler"));
         }
