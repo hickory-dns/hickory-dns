83b192cce069995f2c3bec223c30aebfe459d168
Dirkjan Ochtman <dirkjan@ochtman.nl>
2025-12-09 13:52:32 +0100

    proto: promote http::ErrorKind to http::Error

--- crates/proto/src/http/error.rs
+++ crates/proto/src/http/error.rs
@@ -5,7 +5,7 @@
 // https://opensource.org/licenses/MIT>, at your option. This file may not be
 // copied, modified, or distributed except according to those terms.
 
-use alloc::{fmt, string::String};
+use alloc::string::String;
 use core::num::ParseIntError;
 use std::io;
 
@@ -16,10 +16,10 @@ use thiserror::Error;
 /// An alias for results returned by functions of this crate
 pub type Result<T> = ::core::result::Result<T, Error>;
 
-// TODO: remove this and put in ProtoError
+/// Internal HTTP error type
 #[derive(Debug, Error)]
 #[non_exhaustive]
-pub enum ErrorKind {
+pub enum Error {
     /// Unable to decode header value to string
     #[error("header decode error: {0}")]
     Decode(#[from] ToStrError),
@@ -36,84 +36,30 @@ pub enum ErrorKind {
     #[error("unable to parse number: {0}")]
     ParseInt(#[from] ParseIntError),
 
+    /// A proto error
     #[error("proto error: {0}")]
     ProtoError(#[from] ProtoError),
 
-    #[error("h2: {0}")]
+    /// An HTTP/2 related error
+    #[error("H2: {0}")]
     #[cfg(feature = "__https")]
     H2(#[from] h2::Error),
 
-    #[error("h3: {0}")]
+    /// An HTTP/3 related error
+    #[error("H3: {0}")]
     #[cfg(feature = "__h3")]
     H3(#[from] h3::error::StreamError),
 }
 
-/// The error type for errors that get returned in the crate
-#[derive(Debug)]
-pub struct Error {
-    kind: ErrorKind,
-}
-
-impl Error {
-    /// Get the kind of the error
-    pub fn kind(&self) -> &ErrorKind {
-        &self.kind
-    }
-}
-
-impl fmt::Display for Error {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        f.write_fmt(format_args!("{}", self.kind))
-    }
-}
-
-impl From<ErrorKind> for Error {
-    fn from(kind: ErrorKind) -> Self {
-        Self { kind }
-    }
-}
-
 impl From<&'static str> for Error {
     fn from(msg: &'static str) -> Self {
-        ErrorKind::Message(msg).into()
+        Self::Message(msg)
     }
 }
 
 impl From<String> for Error {
     fn from(msg: String) -> Self {
-        ErrorKind::Msg(msg).into()
-    }
-}
-
-impl From<ParseIntError> for Error {
-    fn from(err: ParseIntError) -> Self {
-        ErrorKind::from(err).into()
-    }
-}
-
-impl From<ToStrError> for Error {
-    fn from(err: ToStrError) -> Self {
-        ErrorKind::from(err).into()
-    }
-}
-
-impl From<ProtoError> for Error {
-    fn from(msg: ProtoError) -> Self {
-        ErrorKind::ProtoError(msg).into()
-    }
-}
-
-#[cfg(feature = "__https")]
-impl From<h2::Error> for Error {
-    fn from(msg: h2::Error) -> Self {
-        ErrorKind::H2(msg).into()
-    }
-}
-
-#[cfg(feature = "__h3")]
-impl From<h3::error::StreamError> for Error {
-    fn from(msg: h3::error::StreamError) -> Self {
-        ErrorKind::H3(msg).into()
+        Self::Msg(msg)
     }
 }
 
