1e62f4ea7e863fb7d109c441b1756d2176b17612
Dirkjan Ochtman <dirkjan@ochtman.nl>
2025-12-09 16:48:59 +0100

    bin: promote ConfigErrorKind to ConfigError

--- bin/src/lib.rs
+++ bin/src/lib.rs
@@ -744,7 +744,7 @@ impl TlsCertConfig {
 /// The error kind for errors that get returned in the crate
 #[derive(Debug, Error)]
 #[non_exhaustive]
-pub enum ConfigErrorKind {
+pub enum ConfigError {
     // foreign
     /// An error got returned from IO
     #[error("io error: {0}")]
@@ -757,36 +757,6 @@ pub enum ConfigErrorKind {
     /// An error occurred while parsing a zone file
     #[error("failed to parse the zone file: {0}")]
     ZoneParse(#[from] ParseError),
-}
-
-/// The error type for errors that get returned in the crate
-#[derive(Debug)]
-pub struct ConfigError {
-    kind: Box<ConfigErrorKind>,
-}
-
-impl ConfigError {
-    /// Get the kind of the error
-    pub fn kind(&self) -> &ConfigErrorKind {
-        &self.kind
-    }
-}
-
-impl fmt::Display for ConfigError {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        f.write_fmt(format_args!("{}", self.kind))
-    }
-}
-
-impl<E> From<E> for ConfigError
-where
-    E: Into<ConfigErrorKind>,
-{
-    fn from(error: E) -> Self {
-        Self {
-            kind: Box::new(error.into()),
-        }
-    }
 }
 
 #[cfg(all(test, any(feature = "resolver", feature = "recursor")))]
