02da938fb1e7138f5f2fe7eef97ba7066c925b51
Dirkjan Ochtman <dirkjan@ochtman.nl>
2025-12-09 14:00:15 +0100

    proto: fold http error module into http root

--- crates/proto/src/http/mod.rs
+++ crates/proto/src/http/mod.rs
@@ -7,17 +7,76 @@
 
 //! HTTP protocol related components for DNS over HTTP/2 (DoH) and HTTP/3 (DoH3)
 
+use core::num::ParseIntError;
+use std::io;
+
+use alloc::string::String;
+use http::header::ToStrError;
 use http::{HeaderMap, HeaderValue};
+use thiserror::Error;
+
+use crate::error::ProtoError;
 
 pub(crate) const MIME_APPLICATION_DNS: &str = "application/dns-message";
 
 /// The default query path for DNS-over-HTTPS if none was given.
 pub const DEFAULT_DNS_QUERY_PATH: &str = "/dns-query";
 
-pub(crate) mod error;
-pub use error::Error;
 pub mod request;
 pub mod response;
+
+/// Internal HTTP error type
+#[derive(Debug, Error)]
+#[non_exhaustive]
+pub enum Error {
+    /// Unable to decode header value to string
+    #[error("header decode error: {0}")]
+    Decode(#[from] ToStrError),
+
+    /// An error with an arbitrary message, referenced as &'static str
+    #[error("{0}")]
+    Message(&'static str),
+
+    /// An error with an arbitrary message, stored as String
+    #[error("{0}")]
+    Msg(String),
+
+    /// Unable to parse header value as number
+    #[error("unable to parse number: {0}")]
+    ParseInt(#[from] ParseIntError),
+
+    /// A proto error
+    #[error("proto error: {0}")]
+    ProtoError(#[from] ProtoError),
+
+    /// An HTTP/2 related error
+    #[error("H2: {0}")]
+    #[cfg(feature = "__https")]
+    H2(#[from] h2::Error),
+
+    /// An HTTP/3 related error
+    #[error("H3: {0}")]
+    #[cfg(feature = "__h3")]
+    H3(#[from] h3::error::StreamError),
+}
+
+impl From<&'static str> for Error {
+    fn from(msg: &'static str) -> Self {
+        Self::Message(msg)
+    }
+}
+
+impl From<String> for Error {
+    fn from(msg: String) -> Self {
+        Self::Msg(msg)
+    }
+}
+
+impl From<Error> for io::Error {
+    fn from(err: Error) -> Self {
+        Self::other(format!("https: {err}"))
+    }
+}
 
 /// Represents a version of the HTTP spec.
 #[derive(Clone, Copy, Debug)]
@@ -47,5 +106,5 @@ impl Version {
 /// on per-request HTTP headers and this trait allows their addition.
 pub trait SetHeaders: Send + Sync + 'static {
     /// Get a set of headers to add to the query
-    fn set_headers(&self, headers: &mut HeaderMap<HeaderValue>) -> Result<(), error::Error>;
+    fn set_headers(&self, headers: &mut HeaderMap<HeaderValue>) -> Result<(), Error>;
 }
