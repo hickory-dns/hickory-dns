c3e5efdadd389cbc68a77fb19d9321fd00a39240
Dirkjan Ochtman <dirkjan@ochtman.nl>
2025-12-10 11:03:13 +0100

    resolver: drop separate lint tweaks inside recursor

--- crates/resolver/src/recursor/mod.rs
+++ crates/resolver/src/recursor/mod.rs
@@ -7,24 +7,6 @@
 
 //! A recursive DNS resolver based on the Hickory DNS (stub) resolver
 
-#![warn(
-    clippy::default_trait_access,
-    clippy::dbg_macro,
-    clippy::print_stdout,
-    clippy::unimplemented,
-    missing_copy_implementations,
-    missing_docs,
-    non_snake_case,
-    non_upper_case_globals,
-    rust_2018_idioms,
-    unreachable_pub
-)]
-#![allow(
-    clippy::single_component_path_imports,
-    clippy::upper_case_acronyms, // can be removed on a major release boundary
-)]
-#![cfg_attr(docsrs, feature(doc_cfg, doc_auto_cfg))]
-
 #[cfg(feature = "serde")]
 use std::{
     borrow::Cow,
