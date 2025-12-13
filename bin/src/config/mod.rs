2bbfcd68d8b0e17fbb44a3dc2995d9acb75e382c
Dirkjan Ochtman <dirkjan@ochtman.nl>
2025-12-13 19:38:51 +0100

    bin: move config.rs to config/mod.rs

--- bin/src/config/mod.rs
+++ bin/src/config/mod.rs
@@ -759,7 +759,7 @@ mod tests {
     #[test]
     fn example_recursor_config() {
         toml::from_str::<Config>(include_str!(
-            "../../tests/test-data/test_configs/example_recursor.toml"
+            "../../../tests/test-data/test_configs/example_recursor.toml"
         ))
         .unwrap();
     }
@@ -768,7 +768,7 @@ mod tests {
     #[test]
     fn example_recursor_opportunistic_enc_config() {
         toml::from_str::<Config>(include_str!(
-            "../../tests/test-data/test_configs/example_recursor_opportunistic_enc.toml"
+            "../../../tests/test-data/test_configs/example_recursor_opportunistic_enc.toml"
         ))
         .unwrap();
     }
