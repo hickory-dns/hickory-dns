592c7f3bb58f91b4a232d42a1fc94c3a5c9246e9
Daniel McCarney <daniel@binaryparadox.net>
2026-01-14 13:02:59 -0500

    server: blocklist entries metric name consistency
    Other similar gauge metrics use "_total" in the name, so let's rename
    "hickory_blocklist_entries" to "hickory_blocklist_entries_total".

--- crates/server/src/metrics.rs
+++ crates/server/src/metrics.rs
@@ -763,7 +763,7 @@ pub mod blocklist {
     impl BlocklistMetrics {
         pub(crate) fn new() -> Self {
             describe_gauge!(
-                ENTRIES,
+                ENTRIES_TOTAL,
                 Unit::Count,
                 "The total number of entries in all configured blocklists",
             );
@@ -789,7 +789,7 @@ pub mod blocklist {
             );
 
             Self {
-                entries: gauge!(ENTRIES),
+                entries: gauge!(ENTRIES_TOTAL),
                 blocked_queries: counter!(BLOCKED_QUERIES_TOTAL),
                 logged_queries: counter!(LOGGED_QUERIES_TOTAL),
                 total_hits: counter!(HITS_TOTAL),
@@ -800,7 +800,7 @@ pub mod blocklist {
 
     /// The total number of entries in all configured blocklists
     #[cfg(feature = "blocklist")]
-    pub const ENTRIES: &str = "hickory_blocklist_list_entries";
+    pub const ENTRIES_TOTAL: &str = "hickory_blocklist_list_entries_total";
 
     /// The total number of requests that were blocked by the blocklist zone handler
     #[cfg(feature = "blocklist")]
