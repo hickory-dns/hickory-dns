e24d75cd1d83640791ff36c3800e9436ca8e1603
Daniel McCarney <daniel@binaryparadox.net>
2026-01-14 10:17:03 -0500

    compatibility-tests: rewrite zone_transfer.rs w/ dns-test
    Like tsig-test.rs, this test can benefit from the dns-test
    infrastructure so manual BIND setup isn't required, and we can extend
    the test to other implementations in the future.

--- conformance/compatibility-tests/src/zone_transfer.rs
+++ conformance/compatibility-tests/src/zone_transfer.rs
@@ -5,21 +5,25 @@
 // https://opensource.org/licenses/MIT>, at your option. This file may not be
 // copied, modified, or distributed except according to those terms.
 
-#![cfg(not(feature = "none"))]
-
 use std::net::{IpAddr, Ipv4Addr, SocketAddr};
 use std::str::FromStr;
 
 use futures::TryStreamExt;
 use time::Duration;
 
-use crate::NamedProcess;
 use hickory_net::client::{Client, ClientHandle};
 use hickory_net::runtime::TokioRuntimeProvider;
 use hickory_net::tcp::TcpClientStream;
 use hickory_net::xfer::DnsMultiplexer;
 use hickory_proto::rr::{Name, RData, Record, RecordType, rdata::A};
 use test_support::subscribe;
+
+use dns_test::{
+    FQDN, Implementation, Network,
+    name_server::{AdditionalZoneConfig, NameServer, ZoneAcl},
+    record::{A as TestA, Record as DnsTestRecord, SOA, SoaSettings},
+    zone_file::ZoneFile,
+};
 
 macro_rules! assert_serial {
     ( $record:expr, $serial:expr  ) => {{
@@ -36,8 +40,53 @@ macro_rules! assert_serial {
 async fn test_zone_transfer() {
     subscribe();
 
-    let (_process, port) = NamedProcess::start();
-    let socket = SocketAddr::new(IpAddr::V4(Ipv4Addr::LOCALHOST), port);
+    let mut ns = NameServer::new(
+        &Implementation::Bind,
+        FQDN::from_str("example.com.").unwrap(),
+        &Network::new().unwrap(),
+    )
+    .unwrap();
+
+    // Create an extra zone file that has too many records to be
+    // returned in a single TCP message.
+    let mut zone_file = ZoneFile::new(SOA {
+        zone: FQDN::from_str("example.net.").unwrap(),
+        ttl: 86400,
+        nameserver: FQDN::from_str("example.net.").unwrap(),
+        admin: FQDN::from_str("root.example.net.").unwrap(),
+        settings: SoaSettings {
+            serial: 20210101,
+            ..SoaSettings::default()
+        },
+    });
+
+    // Add NS record for the zone
+    zone_file.add(DnsTestRecord::ns(
+        FQDN::from_str("example.net.").unwrap(),
+        FQDN::from_str("hickory-dns.org.").unwrap(),
+    ));
+
+    // Add a bunch of mock records to bloat the zone.
+    let bogus_record_count = 2000;
+    for i in 0..bogus_record_count {
+        zone_file.add(TestA {
+            fqdn: FQDN::from_str(&format!("www{i}.example.net.")).unwrap(),
+            ttl: 86400,
+            ipv4_addr: Ipv4Addr::new(127, 0, 0, 1),
+        });
+    }
+
+    // Add the extra zone along with configuration to allow unsigned transfers and updates.
+    ns.add_zone_with_config(
+        FQDN::from_str("example.net.").unwrap(),
+        zone_file,
+        AdditionalZoneConfig::default()
+            .allow_transfer(ZoneAcl::Any)
+            .allow_update(ZoneAcl::Any),
+    );
+
+    let ns = ns.start().unwrap();
+    let socket = SocketAddr::new(IpAddr::V4(ns.ipv4_addr()), 53);
     let (future, sender) =
         TcpClientStream::new(socket, None, None, TokioRuntimeProvider::default());
     let stream = future.await.expect("failed to connect");
@@ -52,10 +101,11 @@ async fn test_zone_transfer() {
         .try_collect::<Vec<_>>()
         .await
         .expect("query failed");
+
     assert_ne!(result.len(), 1);
     assert_eq!(
         result.iter().map(|r| r.answers().len()).sum::<usize>(),
-        2000 + 3
+        bogus_record_count + 3
     );
 
     let RData::SOA(soa) = result[0].answers()[0].data() else {
