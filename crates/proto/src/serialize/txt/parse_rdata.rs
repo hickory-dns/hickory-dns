60bd587179fdb47e465c406a487fae55422547d9
Dirkjan Ochtman <dirkjan@ochtman.nl>
2026-03-27 16:43:05 +0100

    proto: move CERT parser to CERT::from_tokens()

--- crates/proto/src/serialize/txt/parse_rdata.rs
+++ crates/proto/src/serialize/txt/parse_rdata.rs
@@ -10,7 +10,7 @@ use alloc::vec::Vec;
 
 #[cfg(feature = "__dnssec")]
 use crate::dnssec::rdata::DNSSECRData;
-use crate::rr::rdata::{A, AAAA, CAA};
+use crate::rr::rdata::{A, AAAA, CAA, CERT};
 use crate::{
     rr::{
         Name, RData, RecordType,
@@ -71,7 +71,7 @@ impl RDataParser for RData {
             RecordType::ANY => return Err(ParseError::from("parsing ANY doesn't make sense")),
             RecordType::AXFR => return Err(ParseError::from("parsing AXFR doesn't make sense")),
             RecordType::CAA => Self::CAA(CAA::from_tokens(tokens)?),
-            RecordType::CERT => Self::CERT(cert::parse(tokens)?),
+            RecordType::CERT => Self::CERT(CERT::from_tokens(tokens)?),
             RecordType::CNAME => Self::CNAME(CNAME(name::parse(tokens, origin)?)),
             RecordType::CSYNC => csync::parse(tokens).map(Self::CSYNC)?,
             RecordType::HINFO => Self::HINFO(hinfo::parse(tokens)?),
