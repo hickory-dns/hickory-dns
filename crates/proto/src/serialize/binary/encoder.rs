6e70e931515286a701b00ce91b06988eb71e89ef
David Cook <dcook@letsencrypt.org>
2026-04-29 11:38:56 -0500

    Limit number of name pointers stored in BinEncoder

--- crates/proto/src/serialize/binary/encoder.rs
+++ crates/proto/src/serialize/binary/encoder.rs
@@ -267,7 +267,7 @@ impl<'a> BinEncoder<'a> {
         assert!(start <= (u16::MAX as usize));
         assert!(end <= (u16::MAX as usize));
         assert!(start <= end);
-        if self.offset < 0x3FFF_usize {
+        if self.offset < 0x3FFF_usize && self.name_pointers.len() < COMPRESSION_CANDIDATE_LIMIT {
             self.name_pointers
                 .push((start, self.slice_of(start, end).to_vec())); // the next char will be at the len() location
         }
@@ -454,6 +454,13 @@ impl<'a> BinEncoder<'a> {
         }
     }
 }
+
+/// Maximum number of label pointers we will store for later use in name compression.
+///
+/// This is chosen to be a power of two, so that we use the full capacity of the backing vector.
+/// With the current limits, name compression searches make up a small portion of the time it takes
+/// to encode pathologically large messages.
+const COMPRESSION_CANDIDATE_LIMIT: usize = 64;
 
 /// Selects how names should be encoded.
 #[derive(Clone, Copy)]
