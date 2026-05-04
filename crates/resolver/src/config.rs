f8c507e11253d90ee9718062c700a11b253b5326
James Brown <roguelazer@roguelazer.com>
2026-05-04 13:13:16 -0700

    fix docstring for LookupIpStrategy

--- crates/resolver/src/config.rs
+++ crates/resolver/src/config.rs
@@ -720,12 +720,12 @@ pub enum LookupIpStrategy {
     Ipv6Only,
     /// Query for A and AAAA in parallel, ordering A before AAAA
     Ipv4AndIpv6,
-    /// Query for AAAA and A in parallel, ordering AAAA before A
+    /// Query for AAAA and A in parallel, ordering AAAA before A (default)
     #[default]
     Ipv6AndIpv4,
     /// Query for Ipv6 if that fails, query for Ipv4
     Ipv6thenIpv4,
-    /// Query for Ipv4 if that fails, query for Ipv6 (default)
+    /// Query for Ipv4 if that fails, query for Ipv6
     Ipv4thenIpv6,
 }
 
