fcaff593cd6c14d5969007b1ec5dec1e74913aa0
David Cook <dcook@letsencrypt.org>
2026-05-05 10:56:58 -0500

    Add more tests with CNAME at wildcard name

--- conformance/conformance-tests/src/resolver/dns/scenarios/cname.rs
+++ conformance/conformance-tests/src/resolver/dns/scenarios/cname.rs
@@ -488,6 +488,211 @@ fn signed_zone_negative() -> Result<(), Error> {
     Ok(())
 }
 
+/// Tests how a non-validating resolver handles DNSSEC records when following a CNAME alias at a
+/// wildcard name, and getting a positive response.
+#[test]
+#[ignore = "hickory does not include NSEC/NSEC3 records from CNAME response"]
+fn signed_wildcard_cname_positive() -> Result<(), Error> {
+    let (_network, _nameservers, resolver, client) = setup_signed_zone()?;
+    let output = client.dig(
+        *DigSettings::default().recurse().dnssec(),
+        resolver.ipv4_addr(),
+        RecordType::A,
+        &FQDN::EXAMPLE_SUBDOMAIN
+            .push_label("wildcard")
+            .push_label("a"),
+    )?;
+    assert_eq!(output.status, DigStatus::NOERROR, "{output:#?}");
+    assert!(
+        output.answer.iter().any(|record| {
+            let Record::CNAME(CNAME { fqdn, .. }) = record else {
+                return false;
+            };
+            fqdn.as_str() == "a.wildcard.example.hickory-dns.testing."
+        }),
+        "CNAME record is missing {output:#?}"
+    );
+    assert!(
+        output.answer.iter().any(|record| {
+            let Record::RRSIG(RRSIG { fqdn, .. }) = record else {
+                return false;
+            };
+            fqdn.as_str() == "a.wildcard.example.hickory-dns.testing."
+        }),
+        "RRSIG record for CNAME is missing {output:#?}"
+    );
+    assert!(
+        output.answer.iter().any(|record| {
+            let Record::A(A { fqdn, .. }) = record else {
+                return false;
+            };
+            fqdn.as_str() == "host.other.hickory-dns.testing."
+        }),
+        "A record is missing {output:#?}"
+    );
+    assert!(
+        output.answer.iter().any(|record| {
+            let Record::RRSIG(RRSIG { fqdn, .. }) = record else {
+                return false;
+            };
+            fqdn.as_str() == "host.other.hickory-dns.testing."
+        }),
+        "RRSIG record for A is missing {output:#?}"
+    );
+    // Covers the query name:
+    //
+    //   $ nsec3hash -r 1 0 1 - a.wildcard.example.hickory-dns.testing.
+    //   a.wildcard.example.hickory-dns.testing. NSEC3 1 0 1 - GUAC3Q6G2FI6QTTH3JSJ242EFP6R57I2
+    assert!(
+        output.authority.iter().any(|record| {
+            let Record::NSEC3(NSEC3 {
+                fqdn,
+                next_hashed_owner_name,
+                ..
+            }) = record
+            else {
+                return false;
+            };
+            fqdn.as_str() == "8hk47tugdqehn13l08a2l9a7l29q25rj.example.hickory-dns.testing."
+                && next_hashed_owner_name == "L8L2LQT9T5KG7FK253IDLC5534CQ3RNN"
+        }),
+        "NSEC3 record from zone with CNAME is missing {output:#?}"
+    );
+    assert!(
+        output.authority.iter().any(|record| {
+            let Record::RRSIG(RRSIG { fqdn, .. }) = record else {
+                return false;
+            };
+            fqdn.as_str() == "8hk47tugdqehn13l08a2l9a7l29q25rj.example.hickory-dns.testing."
+        }),
+        "RRSIG record for NSEC3 is missing {output:#?}"
+    );
+    Ok(())
+}
+
+/// Tests how a non-validating resolver handles DNSSEC records when following a CNAME alias at a
+/// wildcard name, and getting a negative response.
+#[test]
+#[ignore = "hickory does not include CNAME chain in response"]
+fn signed_wildcard_cname_negative() -> Result<(), Error> {
+    let (_network, _nameservers, resolver, client) = setup_signed_zone()?;
+    let output = client.dig(
+        *DigSettings::default().recurse().dnssec(),
+        resolver.ipv4_addr(),
+        RecordType::A,
+        &FQDN::EXAMPLE_SUBDOMAIN
+            .push_label("wildcard-negative")
+            .push_label("a"),
+    )?;
+    assert_eq!(output.status, DigStatus::NXDOMAIN, "{output:#?}");
+    assert!(
+        output.answer.iter().any(|record| {
+            let Record::CNAME(CNAME { fqdn, target, .. }) = record else {
+                return false;
+            };
+            fqdn.as_str() == "a.wildcard-negative.example.hickory-dns.testing."
+                && target.as_str() == "wrong.other.hickory-dns.testing."
+        }),
+        "CNAME record is missing {output:#?}"
+    );
+    assert!(
+        output.answer.iter().any(|record| {
+            let Record::RRSIG(RRSIG { fqdn, .. }) = record else {
+                return false;
+            };
+            fqdn.as_str() == "a.wildcard-negative.example.hickory-dns.testing."
+        }),
+        "RRSIG record for CNAME is missing {output:#?}"
+    );
+    // Covers the query name:
+    //
+    //   $ nsec3hash -r 1 0 1 - a.wildcard-negative.example.hickory-dns.testing.
+    //   a.wildcard-negative.example.hickory-dns.testing. NSEC3 1 0 1 - 8RLUPEAJKRATE893HJ62EIAUIQCJM676
+    assert!(
+        output.authority.iter().any(|record| {
+            let Record::NSEC3(NSEC3 {
+                fqdn,
+                next_hashed_owner_name,
+                ..
+            }) = record
+            else {
+                return false;
+            };
+            fqdn.as_str() == "8hk47tugdqehn13l08a2l9a7l29q25rj.example.hickory-dns.testing."
+                && next_hashed_owner_name == "L8L2LQT9T5KG7FK253IDLC5534CQ3RNN"
+        }),
+        "NSEC3 record from zone with CNAME is missing {output:#?}"
+    );
+    assert!(
+        output.authority.iter().any(|record| {
+            let Record::RRSIG(RRSIG { fqdn, .. }) = record else {
+                return false;
+            };
+            fqdn.as_str() == "8hk47tugdqehn13l08a2l9a7l29q25rj.example.hickory-dns.testing."
+        }),
+        "RRSIG record for NSEC3 is missing {output:#?}"
+    );
+    // Covers the target of the CNAME record:
+    //
+    //   $ nsec3hash -r 1 0 1 - wrong.other.hickory-dns.testing.
+    //   wrong.other.hickory-dns.testing. NSEC3 1 0 1 - TV5CS5RQP27NUBADJ7H0Q3DE7T7F8VNA
+    assert!(
+        output.authority.iter().any(|record| {
+            let Record::NSEC3(NSEC3 {
+                fqdn,
+                next_hashed_owner_name,
+                ..
+            }) = record
+            else {
+                return false;
+            };
+            fqdn.as_str() == "722c77kkod0lhnnr7d9rnbit9647rdus.other.hickory-dns.testing."
+                && next_hashed_owner_name == "UVQ6A2S4RMV0MRON9KOT3N6TH7RGC8QM"
+        }),
+        "NSEC3 record covering target name is missing {output:#?}"
+    );
+    assert!(
+        output.authority.iter().any(|record| {
+            let Record::RRSIG(RRSIG { fqdn, .. }) = record else {
+                return false;
+            };
+            fqdn.as_str() == "722c77kkod0lhnnr7d9rnbit9647rdus.other.hickory-dns.testing."
+        }),
+        "RRSIG record for NSEC3 is missing {output:#?}"
+    );
+    // Matches the next closest encloser and covers the wildcard at it.
+    //
+    //   $ nsec3hash -r 1 0 1 - other.hickory-dns.testing.
+    //   other.hickory-dns.testing. NSEC3 1 0 1 - UVQ6A2S4RMV0MRON9KOT3N6TH7RGC8QM
+    //   $ nsec3hash -r 1 0 1 - *.other.hickory-dns.testing.
+    //   *.other.hickory-dns.testing. NSEC3 1 0 1 - V5O27Q1MQGOT5GN79U1QIQEM1TKQHNAK
+    assert!(
+        output.authority.iter().any(|record| {
+            let Record::NSEC3(NSEC3 {
+                fqdn,
+                next_hashed_owner_name,
+                ..
+            }) = record
+            else {
+                return false;
+            };
+            fqdn.as_str() == "uvq6a2s4rmv0mron9kot3n6th7rgc8qm.other.hickory-dns.testing."
+                && next_hashed_owner_name == "3F1DNPG4E4GP4AU4IHFBSR2MEJLQTFOC"
+        }),
+        "NSEC3 record for wildcard proof is missing {output:#?}"
+    );
+    assert!(
+        output.authority.iter().any(|record| {
+            let Record::RRSIG(RRSIG { fqdn, .. }) = record else {
+                return false;
+            };
+            fqdn.as_str() == "uvq6a2s4rmv0mron9kot3n6th7rgc8qm.other.hickory-dns.testing."
+        }),
+        "RRSIG record for NSEC3 is missing {output:#?}"
+    );
+    Ok(())
+}
+
 #[allow(clippy::type_complexity)]
 fn setup_signed_zone() -> Result<(Network, Vec<NameServer<Running>>, Resolver, Client), Error> {
     let network = Network::new()?;
@@ -500,6 +705,20 @@ fn setup_signed_zone() -> Result<(Network, Vec<NameServer<Running>>, Resolver, C
     });
     leaf_1_ns.add(CNAME {
         fqdn: FQDN::EXAMPLE_SUBDOMAIN.push_label("alias-negative"),
+        ttl: 3600,
+        target: FQDN::TEST_DOMAIN.push_label("other").push_label("wrong"),
+    });
+    leaf_1_ns.add(CNAME {
+        fqdn: FQDN::EXAMPLE_SUBDOMAIN
+            .push_label("wildcard")
+            .push_label("*"),
+        ttl: 3600,
+        target: FQDN::TEST_DOMAIN.push_label("other").push_label("host"),
+    });
+    leaf_1_ns.add(CNAME {
+        fqdn: FQDN::EXAMPLE_SUBDOMAIN
+            .push_label("wildcard-negative")
+            .push_label("*"),
         ttl: 3600,
         target: FQDN::TEST_DOMAIN.push_label("other").push_label("wrong"),
     });
