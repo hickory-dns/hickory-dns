452a4951cf9d6f7b0e168963030fbc9e70bd3cda
Daniel McCarney <daniel@binaryparadox.net>
2026-05-08 11:46:42 -0400

    net: surface DS-fetch transport errors as ProofErrorKind::Net

--- crates/resolver/src/recursor/tests.rs
+++ crates/resolver/src/recursor/tests.rs
@@ -1053,10 +1053,10 @@ mod metrics {
 
         #[cfg(feature = "__dnssec")]
         {
-            assert_counter_eq(&map, OUTGOING_QUERIES_TOTAL, vec![], 4);
-            assert_counter_eq(&map, CACHE_HIT_TOTAL, vec![], 5);
-            assert_counter_eq(&map, CACHE_MISS_TOTAL, vec![], 2);
-            assert_histogram_sample_count_eq(&map, CACHE_HIT_DURATION, vec![], 3, Unit::Seconds);
+            assert_counter_eq(&map, OUTGOING_QUERIES_TOTAL, vec![], 6);
+            assert_counter_eq(&map, CACHE_HIT_TOTAL, vec![], 7);
+            assert_counter_eq(&map, CACHE_MISS_TOTAL, vec![], 4);
+            assert_histogram_sample_count_eq(&map, CACHE_HIT_DURATION, vec![], 5, Unit::Seconds);
             assert_histogram_sample_count_eq(&map, CACHE_MISS_DURATION, vec![], 1, Unit::Seconds);
 
             // When validating DNSSEC, we should also see DNSSEC-specific metrics.
